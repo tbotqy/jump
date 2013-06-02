4d3ded7ebfe128011769c5f1b30c6e21d0edf77e
tbotqy <tbotqy@gmail.com>
2013-06-03 01:23:00 +0900

    .gitignore is now working

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Tlviewer::Application.config.secret_token = '847dfd7a1f069b2c5868fb5ab34f9eff0dda5148a3bf67890884e365efde0a1e8040c1192f1601ddffdaa90e3ca79519c600ddc63e6d00156b64387b1005a352'
