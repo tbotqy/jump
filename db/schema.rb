c9f06dc342129431e33d07cd085383ebf7b2a134
tbotaq <tbotaq@gmail.com>
2013-06-03 01:22:17 +0900

    generated each controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20130602160613) do
 
   create_table "entities", :force => true do |t|
     t.integer "status_id",              :limit => 8, :null => false
