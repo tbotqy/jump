98806b97bd36814ef6b781e942fdb2cab2150320
tbotaq <tbotaq@gmail.com>
2013-06-03 16:25:28 +0900

    complete

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,3 +1,12 @@
 class Status < ActiveRecord::Base
   # attr_accessible :title, :body
+  has_many :entity
+  
+  def self.get_total_status_num
+    self.count(
+      :conditions => {
+        :pre_saved => false
+      }
+      )
+  end
 end
