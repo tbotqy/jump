d7c4050a652b72c68abbc881fb5a9c2cc7cae531
tbotqy <tbotqy@gmail.com>
2013-06-05 22:33:54 +0900

    moved /users/callback to /logs/login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,39 +15,6 @@ class UsersController < ApplicationController
 
   end
   
-  def callback
-    # called when user was redirected back to our service from twitter.com
-
-    # read acquired access tokens
-    auth = request.env['omniauth.auth']
-
-    # check whether user's twitter account is protected
-    if auth.extra.raw_info.protected
-      render :file => "/shared/_we_are_sorry" and return
-    end
-
-    # check if tokens are acquired correctly
-    access_token = auth.credentials.token
-    access_token_secret = auth.credentials.secret
-    
-    if !access_token
-      abort("failed in acquiring access token")
-    end
-
-    # check if user already exists
-    if User.twitter_id_exists?(auth.uid)
-      # update account with auth
-      User.update_account(auth)
-    else
-      # create new account
-      User.create_account(auth)
-    end
-
-    # log the user in
-    redirect_to :controller => "logs", :action => "login"
-    
-  end
-
   def home_timeline
     raise "welcome!"
   end
