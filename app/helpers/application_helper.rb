0c1334f4569d0e7c7bd2c9719490438e69d0c2b8
tbotaq <tbotaq@gmail.com>
2013-06-09 23:59:55 +0900

    installed twitter-text-rb gem

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 module ApplicationHelper
 
   def title(given_title = nil)
@@ -8,5 +9,16 @@ module ApplicationHelper
       given_title + " - " + base_title
     end
   end
-  
+
+  def calc_tweet_posted_time(dest_unixtime,utc_offset)
+    dest_unixtime += utc_offset
+    
+    dest_year = Time.at(dest_unixtime).strftime('%Y%').to_i
+    current_year = Time.now.strftime('%Y').to_i
+
+    format = current_user > dest_year ? '%Y年%-m月%-d日' : '%-m月%-d日'
+    
+    Time.at(dest_unixtime).strftime( format )
+  end
+
 end
