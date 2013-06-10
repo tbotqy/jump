70c3eab0a1bdfafe74eecf1b08bb37bcf21d0abf
tbotaq <tbotaq@gmail.com>
2013-06-10 18:52:34 +0900

    tweet's timestamps are now relative

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -26,7 +26,7 @@ module ApplicationHelper
   end
 
   def linkify_tweet_body(tweet_body,entities_from_api)
-    #return tweet_body if entities_from_api.empty?
+    return tweet_body if entities_from_api.empty?
    
     # linkify urls obeying Twitter display requirements
     entities_from_api.each do |entity|
@@ -43,4 +43,28 @@ module ApplicationHelper
 
     tweet_body
   end
+
+  def calc_relative_timestamp(dest_unixtime)
+    # calculate how long has it passed since dest_unixtime
+    # and returns timestamp for tweet in appropriate format, that is same with twitter 
+    
+    current_time = Time.now.to_i
+    diff = current_time - dest_unixtime
+
+    if dest_unixtime.between?(1.minute.ago.to_i, current_time) # less than 1 minute
+      return diff.to_s + "秒"
+    elsif dest_unixtime.between?(1.hour.ago.to_i, current_time) # less than 1 hour
+      return (diff / 60).to_s + "分"
+    elsif dest_unixtime.between?(1.day.ago.to_i, current_time) # less than 1 day
+      # obeys view behavior in twitter
+      # 2.4 hour is shown as 2 hour
+      # 2.5 hour is shown as 3 hour
+      return (diff/3600).round.to_s + "時間"
+    elsif dest_unixtime.between?(1.year.ago.to_i, current_time) # less than 1 year
+      return Time.zone.at(dest_unixtime).strftime("%-m月%-d日")
+    else
+      return Time.zone.at(dest_unixtime).strftime("%Y年%-m月%-d日")
+    end
+  end
+
 end
