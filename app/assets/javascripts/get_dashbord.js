baf17cf502631a1816717c61e4f48717d024ef30
tbotaq <tbotaq@gmail.com>
2013-06-15 01:48:08 +0900

    fixed typo

--- app/assets/javascripts/get_dashbord.js
+++ app/assets/javascripts/get_dashbord.js
@@ -8,17 +8,17 @@ $(function(){
   
   // check if dashbord already exists
   if( $("#wrap-term-selectors").size() > 0 ) return;
-
+  
   var elmWholeWrapper = $("#wrap-dashbord");
   // show the loading icon
   elmWholeWrapper.html("<img src=\"/assets/ajax-loader.gif\" alt=\"読込中\" />");
-
+  
   var actionType = getDashbordType();
-
+  
   var sendData = {
-      action_type:actionType
+    action_type:actionType
   };
-
+  
   var elmErrorHtml = $(document.createElement("div")).html("<p>日付ナビゲーションの生成に失敗しました。<br/>画面をリロードして下さい。");
   $.ajax({
     
@@ -27,6 +27,7 @@ $(function(){
     data:sendData,
     dataType:'html',
     success:function(res){
+      
       if(res){
         elmWholeWrapper.html(res);
       }else{
