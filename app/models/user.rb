f5cba890cdccd2cbba6b4ff53b1313fb0da25075
tbotaq <tbotaq@gmail.com>
2013-06-15 20:25:48 +0900

    implemented controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 
-  has_many :statuses, :dependent => :delete_all
+  has_many :statuses, :dependent => :destroy
   has_many :friends, :dependent => :delete_all
   
   def has_friend?
