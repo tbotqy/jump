9b0c4e2a69df1ecd38b1b80a4400e1bbcff1b674
tbotaq <tbotaq@gmail.com>
2013-07-11 02:57:40 +0900

    edit design of global header

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -15,6 +15,23 @@ $(function(){
     data:{"authenticity_token":getCsrfToken()}
   });
 
+  ////////////////////////////
+  // code for global header //
+  ////////////////////////////
+  if( $("#link-timeline").size() > 0 ){
+      
+    // detect current page type and add active class to the button
+    var elmLinkList = $("#link-timeline");
+    var currentActionName = elmLinkList.data("current-action-name");
+    elmLinkList.find("."+currentActionName).addClass("active");
+  
+    // make link active on it is clicked
+    elmLinkList.on("click","li",function(){
+      elmLinkList.find("li").removeClass("active");
+      $(this).addClass("active");
+    });
+
+  }
   
   //////////////////////////
   // code for each status //
