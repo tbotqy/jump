caaf41e1ff3ea39276290ac3efcaa9e418352588
tbotaq <tbotaq@gmail.com>
2016-11-19 14:35:17 +0900

    checkout config dir on master

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+Tlviewer::Application.routes.draw do
+  match "/browsers" => "users#browsers"
+  match "/for_users" => "users#for_users"
+  match "/auth/twitter/callback" => "logs#login"
+  match "/auth/failure" => "logs#logout"
+  match "/statuses/import" => "statuses#import" 
+  match "/your/tweets/(:date)" => "users#sent_tweets"
+  match "/your/home_timeline/(:date)" => "users#home_timeline"
+  match "/your/data" => "users#setting"
+  match "/public_timeline/(:date)" => "users#public_timeline"
+  match "/logout" => "logs#logout"
+  match "/admin/accounts" => "admin#accounts"
+  match "/admin/statuses" => "admin#statuses"
+  match "/sorry" => "logs#sorry"
+  root :to => 'users#index'
+  match ':controller(/:action(/:id))(.:format)'
+  match "*path" => "application#render_404"
 end
