16151d6c6378867235749c54f13d9b924153de7e
tbotqy <tbotqy@gmail.com>
2016-11-27 14:31:13 +0900

    remove unused attr_accessible

--- app/models/stat.rb
+++ app/models/stat.rb
@@ -1,11 +1,10 @@
 class Stat < ActiveRecord::Base
-  attr_accessible :type, :updated_at, :value
   self.inheritance_column = :_type_disabled
 
   def self.increase(dest_type_name,number_to_add)
     # increase the number of destinated data value
     dest_record = self.get_dest_record(dest_type_name)
-    
+
     # add given number
     dest_record.add_value(number_to_add)
   end
@@ -13,7 +12,7 @@ class Stat < ActiveRecord::Base
   def self.decrease(dest_type_name,number_to_subtract)
     # decrease the value of destinated record with given number
     dest_record = self.get_dest_record(dest_type_name)
- 
+
     # substruct given number from the value in destinated record
     dest_record.subtract_value(number_to_subtract)
   end
@@ -31,7 +30,7 @@ class Stat < ActiveRecord::Base
     # add given number to current record
     update_attributes(:value => (self.value + number_to_add))
   end
-  
+
   def subtract_value(number_to_subtract)
     # subtruct given number from current record
     update_attributes(:value => (self.value - number_to_subtract))
@@ -43,5 +42,5 @@ class Stat < ActiveRecord::Base
     self.get_dest_record("active_status_count").update_attributes(:value => count_on_statuses)
   end
 
-  
+
 end
--- app/models/status/broken.rb
+++ app/models/status/broken.rb
@@ -1,5 +1,4 @@
 class Status::Broken < ActiveRecord::Base
-  attr_accessible :created_at, :state, :solved, :status_id, :updated_at
   scope :unsolved, -> {where(:solved => false)}
   scope :invalid_profile_image_url, -> {where(:state => "invalid profile image url")}
 
@@ -20,7 +19,7 @@ class Status::Broken < ActiveRecord::Base
     count_unchecked_retweets = unchecked_retweets.count
     count_invalid = 0
     count_processed = 0
-   
+
     puts "Starting check for #{count_unchecked_retweets} of unchecked retweets..."
     from = 0
     progress_bar_whole = ProgressBar.create(:title => "Detecting invalid prf-img(whole)",:total => count_unchecked_retweets,:format => "%t |%B| %P[%],%a,%E(%c/%C)")
@@ -31,7 +30,7 @@ class Status::Broken < ActiveRecord::Base
       count_limited = unchecked_retweets_limited.count
       #progress_bar_limited = ProgressBar.create(:title => "Detecting invalid prf-img(limited)",:total => count_limited,:format => "%t |%B| %P[%],%a,%E(%c/%C)")
       unchecked_retweets_limited.each.with_index do |unchecked_retweet,index|
-        
+
         uri = URI.parse( URI.encode(unchecked_retweet.rt_profile_image_url_https) )
         begin
           res = Net::HTTP.get_response( uri.host, uri.path )
@@ -53,29 +52,29 @@ class Status::Broken < ActiveRecord::Base
         count_processed += 1
         progress_bar_whole.increment
       end
-      
+
       #puts "Progress at [#{Time.now}] : #{((count_processed.to_f/count_unchecked_retweets.to_f)*100).round(2)} % done. #{count_invalid} statuses were recorded as invalid prof image retweet." if count_processed.modulo(100) == 0
       from += step
     end
-    puts "Complete at #{Time.now} : Recorded #{count_invalid} invalid retweets."    
+    puts "Complete at #{Time.now} : Recorded #{count_invalid} invalid retweets."
   end
 
   def self.fix_invalid_profile_image_url_in_retweet(step = 100)
     puts "Fetch fresh profile image url via API for each retweets in broken list"
     unsolved_list = Status::Broken.unsolved.invalid_profile_image_url.order("id ASC");
-    
+
     count_unsolved =  unsolved_list.count
     count_processed = 0
     count_updated = 0
     count_skipped = 0
-    
+
     twitter = create_twitter_client
     progress_bar_whole = ProgressBar.create(
       :title => "Fetch and update invalid prof-image(whole)",
       :total => count_unsolved,
       :format => "%t |%B| %P[%],%a,%E(%c/%C)"
       )
-    
+
     from = 0
     while( from < count_unsolved )
       limit = "#{from},#{step}"
@@ -83,7 +82,7 @@ class Status::Broken < ActiveRecord::Base
       unsolved_list.limit(limit).each.with_index do |unsolved_state,index|
         dest_status_id_str = Status.find(unsolved_state.status_id).status_id_str
         begin
-          fresh_url = twitter.status(dest_status_id_str).retweet.user.profile_image_url_https 
+          fresh_url = twitter.status(dest_status_id_str).retweet.user.profile_image_url_https
         rescue Twitter::Error::NotFound => error
           unsolved_state.update_state(true,"skipped with NotFoundError")
           #puts "Skipped with NotFoundError : status.id = #{unsolved_state.status_id}."
@@ -95,7 +94,7 @@ class Status::Broken < ActiveRecord::Base
           count_skipped += 1
           next
         rescue Twitter::Error::TooManyRequests => error
-          
+
           progress_bar_whole.log  "Too many requests error occured. Sleep for #{error.rate_limit.reset_in} at #{Time.now} ..."
           #progress_bar_whole.log "Progress: [#{Time.now}] #{((count_processed.to_f/count_unsolved.to_f)*100).round(2)} % of records done. #{count_updated} statuses has been updated."
           sleep error.rate_limit.reset_in
@@ -121,7 +120,7 @@ class Status::Broken < ActiveRecord::Base
     end
     puts "Complete: updated #{count_updated} retweets / skipped (#{count_skipped}) ..."
   end
-  
+
   def update_state(solved_flag,state_string)
     update_attributes(:solved => solved_flag,:state => state_string)
   end
@@ -129,5 +128,5 @@ class Status::Broken < ActiveRecord::Base
   def self.create_twitter_client
     Twitter::Client.new
   end
-  
+
 end
