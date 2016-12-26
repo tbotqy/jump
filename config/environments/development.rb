9ee5d0efdc9a894350611ac8bf99e44d790d1f2f
tbotqy <tbotqy@gmail.com>
2016-12-27 03:00:37 +0900

    delete unnecessary spaces and so on

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,7 +13,7 @@ Jump::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
+  # Don't care if the mailer can't sen1d.
   config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the Rails logger.
