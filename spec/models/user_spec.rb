866d8d2a7e78fa1ec75ff0d11c1c63538cf0ffc3
tbotaq <tbotaq@gmail.com>
2016-12-30 22:32:31 +0900

    added spec for User#has_imported?

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,4 +22,24 @@ describe User do
     end
   end
 
+  describe "#has_imported?" do
+    subject{User.first.has_imported?}
+    context "user has completed initial status import" do
+      before do
+        FactoryGirl.create(:user, :with_initial_import_completed)
+      end
+      it "returns true" do
+        is_expected.to eq true
+      end
+    end
+    context "user hasn't completed initial staus import" do
+      before do
+        FactoryGirl.create(:user, :with_initial_import_incompleted)
+      end
+      it "returns false" do
+        is_expected.to eq false
+      end
+    end
+  end
+
 end
