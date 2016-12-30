6c64adc21f0c165230afc98320f2e3f5709f7769
tbotaq <tbotaq@gmail.com>
2016-12-30 22:20:43 +0900

    added spec for User#has_friend?

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+
+  describe "#has_friend?" do
+    subject{User.first.has_friend?}
+    context "user has some friend" do
+      before do
+        FactoryGirl.create(:user, :with_friend)
+      end
+      it "returns true" do
+        is_expected.to eq true
+      end
+    end
+    context "user has no friend" do
+      before do
+        FactoryGirl.create(:user, :with_no_friend)
+      end
+      it "returns false" do
+        is_expected.to eq false
+      end
+    end
+  end
+
 end
