e806baad9211a9fae6481eddc949b1d1fcaeb333
tbotaq <tbotaq@gmail.com>
2017-05-18 23:30:21 +0900

    bulk selfie

--- app/models/public_date.rb
+++ app/models/public_date.rb
@@ -1,25 +1,25 @@
 class PublicDate < ActiveRecord::Base
-
-  def self.add_record(unixtime_created_at)
-    unless self.date_exists?(unixtime_created_at)
-      self.create(
-        :posted_date => self.convert_time_to_date(unixtime_created_at),
+  class << self
+    def add_record(unixtime_created_at)
+      unless date_exists?(unixtime_created_at)
+        create(
+        :posted_date => convert_time_to_date(unixtime_created_at),
         :posted_unixtime => unixtime_created_at
         )
+      end
+    end
+
+    def date_exists?(unixtime_created_at)
+      date = convert_time_to_date(unixtime_created_at)
+      where(:posted_date => date).exists?
+    end
+
+    def get_list
+      select(:posted_unixtime).order('posted_unixtime DESC')
+    end
+
+    def convert_time_to_date(unixtime_created_at)
+      Time.zone.at(unixtime_created_at).strftime('%Y/%-m/%-d')
     end
   end
-
-  def self.date_exists?(unixtime_created_at)
-    date = self.convert_time_to_date(unixtime_created_at)
-    self.where(:posted_date => date).exists?
-  end
-
-  def self.get_list
-    self.select(:posted_unixtime).order('posted_unixtime DESC')
-  end
-
-  def self.convert_time_to_date(unixtime_created_at)
-    Time.zone.at(unixtime_created_at).strftime('%Y/%-m/%-d')
-  end
-
 end
