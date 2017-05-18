124956e8c293d43163f553f152bf823dc24da676
tbotaq <tbotaq@gmail.com>
2017-05-18 23:31:05 +0900

    stop rocket hashing

--- app/models/public_date.rb
+++ app/models/public_date.rb
@@ -3,15 +3,15 @@ class PublicDate < ActiveRecord::Base
     def add_record(unixtime_created_at)
       unless date_exists?(unixtime_created_at)
         create(
-        :posted_date => convert_time_to_date(unixtime_created_at),
-        :posted_unixtime => unixtime_created_at
+          posted_date: convert_time_to_date(unixtime_created_at),
+          posted_unixtime: unixtime_created_at
         )
       end
     end
 
     def date_exists?(unixtime_created_at)
       date = convert_time_to_date(unixtime_created_at)
-      where(:posted_date => date).exists?
+      where(posted_date: date).exists?
     end
 
     def get_list
