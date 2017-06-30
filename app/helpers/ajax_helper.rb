6818b18c1150660883fad9cc5f2ef5f3b7143247
tbotqy <tbotqy@gmail.com>
2017-06-30 22:43:30 +0900

    move class def to helper

--- app/helpers/ajax_helper.rb
+++ app/helpers/ajax_helper.rb
@@ -1,2 +1,13 @@
 module AjaxHelper
+  def dashbord_class_name(index, max)
+    return 'first last' if max == 1
+    case index
+    when 1
+      'first'
+    when max
+      'last'
+    else
+      'mid'
+    end
+  end
 end
