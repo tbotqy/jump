780470936abfeaf6b8d2ebc3f566fd9e519e224d
tbotaq <tbotaq@gmail.com>
2017-07-16 21:59:52 +0900

    Revert "because secret.yml is not readable in travis ci"
    This reverts commit 413a56d0998610658a45edc73e63f31d0a27a9da.

--- config/initializers/configatron.rb
+++ config/initializers/configatron.rb
@@ -1,2 +1,2 @@
 require 'configatron'
-Configatron::Integrations::Rails.init unless Rails.env.test? # travis ci で rspec 実行時に secret.yml が読み込めないのを回避（git ignoredな為）
+Configatron::Integrations::Rails.init
