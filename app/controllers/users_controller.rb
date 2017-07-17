0b4d6d253c999659996ff5892926bb84c2c795b5
tbotaq <tbotaq@gmail.com>
2017-07-17 18:46:03 +0900

    delegate view logics to view object

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,17 +5,7 @@ class UsersController < ApplicationController
 
   def setting
     @title = "データ管理"
-    @count_statuses = @current_user.statuses.count
-    @count_friends = @current_user.friends.count
-    @status_updated_at = Time.zone.at(@current_user.statuses_updated_at).strftime('%F %T')
-
-    @friend_updated_at = @current_user.friends_updated_at
-    if @friend_updated_at == 0
-      @friend_updated_at = "---"
-    else
-      @friend_updated_at = Time.zone.at(@friend_updated_at).strftime('%F %T')
-    end
-    @profile_updated_at = Time.zone.at(@current_user.updated_at).strftime('%F %T')
+    @view_object = UsersViewObject::Setting.new(@current_user)
   end
 
   def delete_account
