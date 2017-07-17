58e32bc382d2676360ae2e06fac4088290948652
tbotaq <tbotaq@gmail.com>
2017-07-17 17:27:09 +0900

    move the action for service top page to pages_controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
 class PagesController < ApplicationController
+
+  def index
+    return redirect_to(action: :sent_tweets) if logged_in?
+
+    @total_user_num   = User.active.count
+    @total_status_num = DataSummary.active_status_count
+  end
+
   def for_users
   end
 
