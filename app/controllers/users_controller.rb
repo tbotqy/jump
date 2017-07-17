f7239a0815690bc6ebcdc5a5d69782b942a00c85
tbotqy <tbotqy@gmail.com>
2017-07-17 18:56:05 +0900

    move title def to view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,6 @@ class UsersController < ApplicationController
   before_filter :check_tweet_import
 
   def setting
-    @title = "データ管理"
     @view_object = UsersViewObject::Setting.new(@current_user)
   end
 
