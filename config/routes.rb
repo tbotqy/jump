cad4714cfde16c515fe7e093b3115690f299a292
tbotqy <tbotqy@gmail.com>
2018-01-13 01:13:55 +0900

    get rid of rocket expressions

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,18 @@ Jump::Application.routes.draw do
   get "/for_users", to: "pages#for_users"
   get "/browsers", to: "pages#browsers"
   get "/sorry", to: "pages#sorry"
-  get "/auth/twitter/callback" => "session#login"
-  get "/auth/failure" => "session#logout"
-  get "/statuses/import" => "statuses#import"
-  get "/your/tweets/(:date)" => "statuses#sent_tweets"
-  get "/your/home_timeline/(:date)" => "statuses#home_timeline"
-  get "/your/data" => "users#setting"
-  get "/public_timeline/(:date)" => "statuses#public_timeline"
-  get "/logout" => "session#logout"
-  get "/admin/accounts" => "admin#accounts"
-  get "/admin/statuses" => "admin#statuses"
+  get "/auth/twitter/callback", to: "session#login"
+  get "/auth/failure", to: "session#logout"
+  get "/statuses/import", to: "statuses#import"
+  get "/your/tweets/(:date)", to: "statuses#sent_tweets"
+  get "/your/home_timeline/(:date)", to: "statuses#home_timeline"
+  get "/your/data", to: "users#setting"
+  get "/public_timeline/(:date)", to: "statuses#public_timeline"
+  get "/logout", to: "session#logout"
+  get "/admin/accounts", to: "admin#accounts"
+  get "/admin/statuses", to: "admin#statuses"
   post ':controller(/:action(/:id))(.:format)'
-  root :to => 'pages#service_top'
+  root to: 'pages#service_top'
   get ':controller(/:action(/:id))(.:format)'
-  get "*path" => "application#render_404"
+  get "*path", to: "application#render_404"
 end
