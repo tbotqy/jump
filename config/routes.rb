8e8351feac9376f68ffdf07f97459486294dba24
tbotqy <tbotqy@gmail.com>
2018-01-13 01:23:09 +0900

    replace double quotes with sigle ones

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,24 @@
 Jump::Application.routes.draw do
   root to: 'pages#service_top'
-  get "/for_users", to: "pages#for_users"
-  get "/browsers",  to: "pages#browsers"
-  get "/sorry",     to: "pages#sorry"
+  get '/for_users', to: 'pages#for_users'
+  get '/browsers',  to: 'pages#browsers'
+  get '/sorry',     to: 'pages#sorry'
 
-  get "/auth/twitter/callback", to: "session#login"
-  get "/auth/failure",          to: "session#logout"
-  get "/logout",                to: "session#logout"
+  get '/auth/twitter/callback', to: 'session#login'
+  get '/auth/failure',          to: 'session#logout'
+  get '/logout',                to: 'session#logout'
 
-  get "/your/data", to: "users#setting"
+  get '/your/data', to: 'users#setting'
 
-  get "/statuses/import",            to: "statuses#import"
-  get "/your/tweets/(:date)",        to: "statuses#sent_tweets"
-  get "/your/home_timeline/(:date)", to: "statuses#home_timeline"
-  get "/public_timeline/(:date)",    to: "statuses#public_timeline"
+  get '/statuses/import',            to: 'statuses#import'
+  get '/your/tweets/(:date)',        to: 'statuses#sent_tweets'
+  get '/your/home_timeline/(:date)', to: 'statuses#home_timeline'
+  get '/public_timeline/(:date)',    to: 'statuses#public_timeline'
 
-  get "/admin/accounts", to: "admin#accounts"
-  get "/admin/statuses", to: "admin#statuses"
+  get '/admin/accounts', to: 'admin#accounts'
+  get '/admin/statuses', to: 'admin#statuses'
 
   post ':controller(/:action(/:id))(.:format)'
   get  ':controller(/:action(/:id))(.:format)'
-  get  "*path", to: "application#render_404"
+  get  '*path', to: 'application#render_404'
 end
