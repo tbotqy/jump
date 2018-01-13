cad893449e0bbc66bc3b9956a5fff96afa015837
tbotaq <tbotaq@gmail.com>
2018-01-14 02:38:09 +0900

    write route spec for session handling

---
+++ spec/routing/sessions_route_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+describe "routings to sessions"do
+
+  describe "get /auth/twitter/callback" do
+    it do
+      expect(get: "/auth/twitter/callback").to route_to(
+        controller: "session",
+        action: "login"
+      )
+    end
+  end
+
+  describe "get /auth/failure" do
+    it do
+      expect(get: "/auth/failure").to route_to(
+        controller: "session",
+        action: "logout"
+      )
+    end
+  end
+
+  describe "get /logout" do
+    it do
+      expect(get: "/logout").to route_to(
+        controller: "session",
+        action: "logout"
+      )
+    end
+  end
+end
