de4e5654777f1822ded31798ec81372347e5bea0
tbotqy <tbotqy@gmail.com>
2018-01-14 03:54:59 +0900

    write route spec for ajax requests

---
+++ spec/routing/ajax_route_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+describe "routings to ajax" do
+  it{ expect(post: "/ajax/check_profile_update").to route_to("ajax#check_profile_update") }
+  it{ expect(post: "/ajax/check_status_update").to  route_to("ajax#check_status_update") }
+  it{ expect(post: "/ajax/check_friend_update").to  route_to("ajax#check_friend_update") }
+  it{ expect(post: "/ajax/deactivate_account" ).to  route_to("ajax#deactivate_account") }
+  it{ expect(post: "/ajax/delete_account"     ).to  route_to("ajax#delete_account") }
+  it{ expect(post: "/ajax/delete_status"      ).to  route_to("ajax#delete_status") }
+  it{ expect(post: "/ajax/update_status"      ).to  route_to("ajax#update_status") }
+  it{ expect(post: "/ajax/read_more"          ).to  route_to("ajax#read_more") }
+  it{ expect(get:  "/ajax/get_dashbord"       ).to  route_to("ajax#get_dashbord") }
+  it{ expect(post: "/ajax/acquire_statuses"   ).to  route_to("ajax#acquire_statuses") }
+  it{ expect(get:  "/ajax/switch_term"        ).to  route_to("ajax#switch_term") }
+end
