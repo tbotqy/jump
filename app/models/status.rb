f5387b316462633f398613a8b00e09f1699f07f7
tbotqy <tbotqy@gmail.com>
2018-02-20 17:20:10 +0900

    note FIXME

--- app/models/status.rb
+++ app/models/status.rb
@@ -3,7 +3,11 @@ class Status < ActiveRecord::Base
   belongs_to :user
   has_many :entities, dependent: :delete_all
   scope :showable , -> {where(pre_saved: false, deleted_flag: false)}
+
+  # FIXME : this scope 'retweet' is only used in this class itself.
+  # consider to delete this scope and replace with some private method.
   scope :retweet , -> {where(is_retweet: true)}
+
   scope :order_for_timeline , ->{order("twitter_created_at_reversed ASC","status_id_str_reversed ASC")}
   scope :order_for_date_list, ->{order("twitter_created_at_reversed ASC")}
   scope :use_index , ->(index_name) {from("#{table_name} USE INDEX(#{index_name})")}
