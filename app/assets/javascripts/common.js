720bcbd13d2f121de5b42fab6bc286d8d443e28f
tbotaq <tbotaq@gmail.com>
2018-04-23 20:13:09 +0900

    modularize click events for read more button

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -45,45 +45,6 @@ $(function(){
   // code for each status //
   //////////////////////////
 
-  // click action for read more button
-  $("#wrap-timeline-lower").on("click","#read-more",function(e){
-
-    var self = $(this);
-
-    e.preventDefault();
-    var distance = self.offset().top;
-
-    // let button say 'loading'
-    self.button('loading');
-    var elmOldestTweetId = $(".oldest-tweet-id");
-    var oldestTweetId = elmOldestTweetId.val();
-    // fetch more statuses to show
-    $.ajax({
-
-      type:"POST",
-      dataType:"html",
-      data:{
-        "oldest_tweet_id":oldestTweetId,
-        "destination_action_type":detectActionType(location.pathname)
-      },
-      url: '/ajax/read_more',
-      success: function(responce){
-        // remove the element representing last status's timestamp
-        elmOldestTweetId.remove();
-        $("#wrap-read-more").remove();
-
-        // insert loaded html code
-        $(".wrap-one-result:last").after(responce);
-      },
-      error: function(responce){
-        alert("読み込みに失敗しました。");
-      },
-      complete: function(){
-        scrollDownToDestination(e,distance);
-      }
-    });
-  });
-
   var wrap_progress_bar = $(".wrap-progress-bar");
   var import_button = $("#start-import");
 
