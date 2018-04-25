d0f98a512878502bf16d75b796b79aa08068e37b
tbotaq <tbotaq@gmail.com>
2018-04-25 19:55:14 +0900

    modularize codes for title update, with related functions included

--- app/assets/javascripts/functions.js
+++ app/assets/javascripts/functions.js
@@ -6,66 +6,6 @@ function facebook(d, s, id) {
   fjs.parentNode.insertBefore(js, fjs);
 }
 
-function updatePageTitleForTimeline(dateWithHyphen,actionType){
-  var title = "";
-  var baseTitle = "";
-  var dateInJapanese = convertDateToJapanese(dateWithHyphen);
-  var name  = getTimelineOwnerName();
-  var serviceName = "TwitJump";
-  switch(actionType){
-  case "tweets":
-    baseTitle = " " + name +" さんのツイート";
-    break;
-  case "home_timeline":
-    baseTitle = " " + name + " さんのホームタイムライン";
-    break;
-  case "public_timeline":
-    baseTitle = "パブリックタイムライン";
-    break;
-  }
-
-  // concatenate service name to base title
-  baseTitle += " - "+serviceName;
-
-  if(!dateInJapanese){
-    title = baseTitle;
-  }else{
-    // concatenate date string to the head of baseTitle
-    title = dateInJapanese+"の"+baseTitle;
-  }
-
-  // update page title
-  $("title").text(title);
-}
-
-function getTimelineOwnerName(){
-  // returns the string of name(@screen_name)
-  var name = $(':hidden[name="timeline-owner-name"]').val() || "Hey!";
-  var screen_name = $(':hidden[name="timeline-owner-screen-name"]').val() || "Hoo!";
-  return name+"(@"+screen_name+")";
-}
-
-function convertDateToJapanese(dateWithHyphen){
-  // convert given date to 年月日
-  if(!dateWithHyphen || dateWithHyphen == "notSpecified") return false;
-
-  var ret = "";
-  dateWithHyphen.split(/-/).forEach(function(date,index){
-    switch(index){
-      case 0:
-      ret += date+"年";
-      break;
-      case 1:
-      ret += date+"月";
-      break;
-      case 2:
-      ret += date+"日";
-      break;
-    }
-  });
-  return ret;
-}
-
 function getUserAgent(){
 
   var userAgent = window.navigator.userAgent.toLowerCase();
@@ -686,7 +626,7 @@ function ajaxSwitchTerm(date,action_type,mode){
       }
 
       // update page title
-      updatePageTitleForTimeline(date,action_type);
+      PageTitle.updateByDateAndAction(date,action_type);
 
     }
 
