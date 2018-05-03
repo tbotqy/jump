920819420dd557a9a0460af26f7d9cda684ab22f
tbotaq <tbotaq@gmail.com>
2018-05-03 09:59:50 +0900

    add module to handle with click events on timeline nav, which is only called by smart phone access

--- app/assets/javascripts/statuses/home_timeline_sp.js
+++ app/assets/javascripts/statuses/home_timeline_sp.js
@@ -1,6 +1,7 @@
 $(function(){
   Popstate.handleWithSp = true;
   Popstate.bindEvent();
+  TimelineNav.bindClickEvent();
   GlobalHeader.bindClickEventForDropdown();
   Dashbord.handleWithSp = true;
   Dashbord.fetchDashbord();
