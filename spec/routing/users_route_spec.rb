26597a9fcb5daed04de5c667ac811858e543163a
tbotaq <tbotaq@gmail.com>
2018-05-04 23:08:01 +0900

    fill lacked routing

--- spec/routing/users_route_spec.rb
+++ spec/routing/users_route_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
 describe "routings to users" do
   it{ expect(get: "/your/data").to route_to("users#setting")}
+  it{ expect(get: "/users/delete_account").to route_to("users#delete_account")}
 end
