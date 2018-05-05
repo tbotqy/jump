cdd4ca07d5413dfea6248e979ec9668c091428b3
tbotaq <tbotaq@gmail.com>
2018-05-06 04:12:59 +0900

    include js files to the view of home_timeline_sp

--- app/assets/javascripts/statuses/home_timeline_sp.js
+++ app/assets/javascripts/statuses/home_timeline_sp.js
@@ -1,11 +1,16 @@
 $(function(){
   Popstate.handleWithSp = true;
   Popstate.bindEvent();
+
   TimelineNav.bindClickEvent();
+
   GlobalHeader.bindClickEventForDropdown();
+
   Dashbord.handleWithSp = true;
   Dashbord.fetchDashbord();
+
   TweetCard.bindClickEvents();
+
   ReadMore.handleWithSp = true;
   ReadMore.bindClickEvents();
 });
