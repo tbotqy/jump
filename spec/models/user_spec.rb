4b1f2be1d8f90f4679eab02eaac6cadef71eee94
tbotaq <tbotaq@gmail.com>
2018-05-13 14:35:13 +0900

    remove initialized_flag from spec/factory

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,27 +6,6 @@ RSpec.configure do |c|
 end
 
 describe User do
-
-  describe "#has_imported?" do
-    subject{User.first.has_imported?}
-    context "user has completed initial status import" do
-      before do
-        FactoryBot.create(:user, :with_initial_import_completed)
-      end
-      it "returns true" do
-        is_expected.to eq true
-      end
-    end
-    context "user hasn't completed initial staus import" do
-      before do
-        FactoryBot.create(:user, :with_initial_import_incompleted)
-      end
-      it "returns false" do
-        is_expected.to eq false
-      end
-    end
-  end
-
   describe "#has_any_status?" do
     subject{User.first.has_any_status?}
     context "user has any status" do
