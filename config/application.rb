0426ced9058d78bedf5683f558732b50e31a7ea0
tbotaq <tbotaq@gmail.com>
2018-05-15 17:40:55 +0900

    add newly generated files by update command

--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,6 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
-# Pick the frameworks you want:
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -13,6 +8,9 @@ Bundler.require(*Rails.groups)
 
 module Jump
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    # config.load_defaults 5.2
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
