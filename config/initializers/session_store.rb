ce639bd783ce62bcd03a0777ac5ed29ab82a344e
tbotaq <tbotaq@gmail.com>
2018-05-15 17:53:59 +0900

    leave a note

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
 Jump::Application.config.session_store :cookie_store, key: '_jump_session'
+
+# MEMO : this file remained when 4 to 5.0 update and seems to be removed from 5.1
