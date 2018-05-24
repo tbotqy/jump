769e0a9f08e029ffe0fabdab1196add9a7c0d6bc
tbotaq <tbotaq@gmail.com>
2018-05-24 13:21:03 +0900

    implement the view object which represents term selector

---
+++ app/view_objects/ajax_view_object/term_selector/day_list/li_tag.rb
@@ -0,0 +1,34 @@
+module AjaxViewObject
+  class TermSelector
+    class DayList::LiTag
+      delegate :year, :month, :day, to: :@target_day
+
+      def initialize(target_day:, index:, link_path_without_date:, total_li_count_in_ul:)
+        @target_day = target_day
+        @index = index
+        @link_path_without_date = link_path_without_date
+        @total_li_count_in_ul = total_li_count_in_ul
+      end
+
+      def data_date_value
+        "#{year}-#{month}-#{day}"
+      end
+
+      def anchor_text
+        day
+      end
+
+      def anchor_href
+        "#{@link_path_without_date}/#{year}-#{month}-#{day}"
+      end
+
+      def data_complete_text_value
+        day
+      end
+
+      def anchor_class_text
+        AjaxViewObject::TermSelector.li_tag_class_text(index: @index, total_li_count_in_ul: @total_li_count_in_ul)
+      end
+    end
+  end
+end
