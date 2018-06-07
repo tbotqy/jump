9de79a36357f48b7ae7921fc5a62d291557d8844
tbotaq <tbotaq@gmail.com>
2018-06-07 13:40:21 +0900

    delete unused methods

--- app/models/following_twitter_id.rb
+++ app/models/following_twitter_id.rb
@@ -3,22 +3,6 @@ class FollowingTwitterId < ApplicationRecord
   scope :user_id, ->(user_id){where(user_id: user_id)}
 
   class << self
-    def get_friend_user_ids(user_id)
-      # returns the array of friends' user_ids
-
-      # retrieve twitter ids
-      following_twitter_ids = get_friend_twitter_ids(user_id)
-
-      # retrieve user ids by twitter ids
-      user_ids = User.where("twitter_id IN (?)",following_twitter_ids).pluck(:id)
-    end
-
-    def get_friend_twitter_ids(user_id)
-      # returns the array of friends' twitter id
-      select(:following_twitter_id).where(user_id: user_id).pluck(:following_twitter_id)
-    end
-
-
     def save_friends(user_id, friend_ids)
       created_at = Time.now.to_i
 
@@ -32,13 +16,6 @@ class FollowingTwitterId < ApplicationRecord
 
       # update the time stamp in User model
       User.find(user_id).update_attribute(:friends_updated_at, created_at)
-    end
-
-    def update_list(user_id, friend_ids)
-      # delete user's friend list
-      where(user_id: user_id).destroy_all
-      # insert new friend list
-      save_friends(user_id, friend_ids)
     end
   end
 end
