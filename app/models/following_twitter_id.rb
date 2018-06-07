66a29650883eb3b143cb063d6098f17c3d463785
tbotaq <tbotaq@gmail.com>
2018-06-07 13:52:28 +0900

    make some refactoring

--- app/models/following_twitter_id.rb
+++ app/models/following_twitter_id.rb
@@ -4,16 +4,14 @@ class FollowingTwitterId < ApplicationRecord
 
   class << self
     def register!(user_id, following_twitter_ids)
-      following_twitter_ids.each do |following_twitter_id|
+      Array.wrap(following_twitter_ids).each do |following_twitter_id|
         create!(
           user_id: user_id,
-          following_twitter_id: following_twitter_id,
-          created_at: created_at
+          following_twitter_id: following_twitter_id
         )
       end
 
-      # update the time stamp in User model
-      User.find(user_id).update_attribute(:friends_updated_at, created_at)
+      User.find(user_id).update_attribute(:friends_updated_at, Time.now)
     end
   end
 end
