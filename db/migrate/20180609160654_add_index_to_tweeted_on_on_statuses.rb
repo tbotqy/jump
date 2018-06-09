be1e21a838e567632cb51e206b66317ad104e33a
tbotaq <tbotaq@gmail.com>
2018-06-10 01:36:03 +0900

    try on another index

--- db/migrate/20180609160654_add_index_to_tweeted_on_on_statuses.rb
+++ db/migrate/20180609160654_add_index_to_tweeted_on_on_statuses.rb
@@ -1,5 +1,5 @@
 class AddIndexToTweetedOnOnStatuses < ActiveRecord::Migration[5.0]
   def change
-    add_index :statuses, :tweeted_on
+    add_index :statuses, [:deleted, :private, :tweeted_on]
   end
 end
