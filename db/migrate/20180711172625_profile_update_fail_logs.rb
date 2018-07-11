b0c67f68dc1f928cc8a322c499a20d85fea53033
tbotaq <tbotaq@gmail.com>
2018-07-12 03:08:38 +0900

    implement migration

--- db/migrate/20180711172625_profile_update_fail_logs.rb
+++ db/migrate/20180711172625_profile_update_fail_logs.rb
@@ -1,4 +1,10 @@
 class ProfileUpdateFailLogs < ActiveRecord::Migration[5.0]
   def change
+    create_table :profile_update_fail_logs do |t|
+      t.integer :user_id, default: nil, null: false
+      t.string :error_message, default: nil, null: false
+
+      t.timestamps null: false
+    end
   end
 end
