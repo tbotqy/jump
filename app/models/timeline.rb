a0d659b71b06a8f65cf9d46cb54f7be7b9b1117d
tbotaq <tbotaq@gmail.com>
2018-07-15 14:05:01 +0900

    add front class of timeline model classes

---
+++ app/models/timeline.rb
@@ -0,0 +1,48 @@
+class Timeline
+  class << self
+    def user_timeline(date: ,timeline_owner:)
+      by_type_and_date(
+        type: TimelineType.new("user_timeline"),
+        date: date,
+        timeline_owner: timeline_owner
+      )
+    end
+
+    def home_timeline(date: ,timeline_owner:)
+      by_type_and_date(
+        type: TimelineType.new("home_timeline"),
+        date: date,
+        timeline_owner: timeline_owner
+      )
+    end
+
+    def public_timeline(date:)
+      by_type_and_date(
+        type: TimelineType.new("public_timeline"),
+        date: date,
+        timeline_owner: nil
+      )
+    end
+
+    def read_more(type: ,prev_oldest_tweet_id: ,timeline_owner:)
+      target_timeline(type).new(date_string: nil, largest_tweet_id: prev_oldest_tweet_id - 1, timeline_owner: timeline_owner)
+    end
+
+    def by_type_and_date(type: ,date: ,timeline_owner:)
+      target_timeline(type).new(date_string: date, largest_tweet_id: nil, timeline_owner: timeline_owner)
+    end
+
+    private
+
+    def target_timeline(timeline_type)
+      case
+      when timeline_type.public_timeline?
+        Timeline::PublicTimeline
+      when timeline_type.home_timeline?
+        Timeline::HomeTimeline
+      when timeline_type.user_timeline?
+        Timeline::UserTimeline
+      end
+    end
+  end
+end
