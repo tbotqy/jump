fe9660a45fb3c619481948e2674500f192823818
tbotaq <tbotaq@gmail.com>
2018-11-24 20:43:14 +0900

    use provided method to get user signed in

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,7 +2,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def twitter
     User.register_or_update!(auth)
     user = User.find_active_with_auth(auth)
-    login!(user)
+    sign_in user
     return redirect_to controller: :statuses, action: :user_timeline if user.finished_initial_import?
     redirect_to controller: :statuses, action: :import
   end
