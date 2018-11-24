e2956a392514a05b36a1296343d94eb10b4d5bf5
tbotaq <tbotaq@gmail.com>
2018-11-24 20:44:09 +0900

    fix redirect to dest

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,8 +3,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     User.register_or_update!(auth)
     user = User.find_active_with_auth(auth)
     sign_in user
-    return redirect_to controller: :statuses, action: :user_timeline if user.finished_initial_import?
-    redirect_to controller: :statuses, action: :import
+    return redirect_to user_timeline_path if user.finished_initial_import?
+    redirect_to statuses_import_path
   end
 
   private
