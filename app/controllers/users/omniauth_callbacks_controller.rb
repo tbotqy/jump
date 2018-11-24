1ce92f0e22b7e841da064177d1f361ebf28d55e4
tbotaq <tbotaq@gmail.com>
2018-11-24 20:15:11 +0900

    move logics that have already been implemented as callback handler in session_controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,2 +1,16 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def twitter
+    User.register_or_update!(auth)
+    user = User.find_active_with_auth(auth)
+    login!(user)
+    return redirect_to controller: :statuses, action: :user_timeline if user.finished_initial_import?
+    redirect_to controller: :statuses, action: :import
+  end
+
+  private
+
+  # read acquired access tokens
+  def auth
+    @auth ||= request.env['omniauth.auth']
+  end
 end
