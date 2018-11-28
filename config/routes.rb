a2b75898e6560586eb3071438a4302b8659cbd23
tbotaq <tbotaq@gmail.com>
2018-11-28 23:55:32 +0900

    define routings for sign_in/sign_out

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Jump::Application.routes.draw do
   end
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_scope :user do
+    get 'sign_in',  to: 'devise/sessions#new',     as: :new_user_session
+    get 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
+  end
 
   root to: 'pages#service_top'
   get '/for_users', to: 'pages#for_users'
