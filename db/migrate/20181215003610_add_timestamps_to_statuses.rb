1fc9c672fa8710573727b9c0cae84a51bd2dfa69
tbotaq <tbotaq@gmail.com>
2018-12-15 12:38:35 +0900

    add default value

--- db/migrate/20181215003610_add_timestamps_to_statuses.rb
+++ db/migrate/20181215003610_add_timestamps_to_statuses.rb
@@ -1,5 +1,5 @@
 class AddTimestampsToStatuses < ActiveRecord::Migration[5.0]
   def change
-    add_timestamps :statuses, null: true
+    add_timestamps :statuses, null: false, default: -> { 'NOW()' }
   end
 end
