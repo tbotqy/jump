ccc3bf862bb0ca974d5e078480dfd93fb18fb1f0
tbotaq <tbotaq@gmail.com>
2019-05-01 23:31:41 +0900

    delete stale code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,6 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery
 
-  before_action :apply_user_time_zone
   rescue_from Exception, with: :render_500 if Rails.env.production?
 
   def render_404(exception = nil)
@@ -22,14 +21,6 @@ class ApplicationController < ActionController::Base
       Raven.capture_exception(e)
     end
     render file: "errors/#{status_code}", status: status_code, layout: "error", content_type: "text/html"
-  end
-
-  def apply_user_time_zone
-    if current_user
-      Time.zone = current_user.time_zone
-    else
-      Time.zone = cookies[:timezone] || "UTC"
-    end
   end
 
   def check_tweet_import
