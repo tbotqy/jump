752e4059748720900ff2ae077978795a70fdd944
tbotaq <tbotaq@gmail.com>
2019-06-14 12:27:43 +0900

    bin/rails app:update with rails 5.1

--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,16 @@
-# frozen_string_literal: true
+require_relative 'boot'
 
-require_relative "boot"
-
-require "rails/all"
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "action_cable/engine"
+# require "sprockets/railtie"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,33 +19,15 @@ Bundler.require(*Rails.groups)
 module Jump
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    # config.load_defaults 5.2
+    config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    # Only loads a smaller set of middleware suitable for API only apps.
+    # Middleware like session, flash, cookies can be added back manually.
+    # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    config.time_zone = "Tokyo"
-    config.active_record.default_timezone = :utc
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
-
-
-    # Enable the asset pipeline
-    config.assets.enabled = true
-    # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = "1.0"
-    # add fonts dir for font awesome
-    config.assets.paths << Rails.root.join("app", "assets", "fonts")
-    config.generators.template_engine = :slim
-
-    config.active_job.queue_adapter = :sidekiq
-    config.autoload_paths += %W(#{config.root}/config/constraints)
   end
 end
