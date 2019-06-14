f27ea02177df083f1737ed4c31dea92f60f4c2ea
tbotaq <tbotaq@gmail.com>
2019-06-14 11:25:24 +0900

    Routing must be changed

--- spec/routing/pages_route_spec.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require "rails_helper"
-describe "routings to static pages" do
-  it { expect(get: "/").to route_to("pages#service_top") }
-  it { expect(get: "/for_users").to route_to("pages#for_users") }
-end
