8f06ee7a435b6b694dcbb9b02d33231145d49d75
tbotaq <tbotaq@gmail.com>
2019-06-14 12:27:43 +0900

    bin/rails app:update with rails 5.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
-
-Jump::Application.routes.draw do
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
