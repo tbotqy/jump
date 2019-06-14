b7b470b6e29519996c1758d305fd75d2d2d2f313
tbotaq <tbotaq@gmail.com>
2019-06-14 11:25:24 +0900

    Routing must be changed

--- spec/routing/users_route_spec.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require "rails_helper"
-describe "routings to users" do
-  it { expect(get: "/users/setting").to route_to("users#setting") }
-  it { expect(get: "/users/delete_account").to route_to("users#delete_account") }
-end
