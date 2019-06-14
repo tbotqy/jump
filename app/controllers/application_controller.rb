dd9dd08b90e69c611b6670292a694591231a4541
tbotaq <tbotaq@gmail.com>
2019-06-14 10:39:19 +0900

    Change from which class to inherit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
   include Jpmobile::ViewSelector
 
   protect_from_forgery
