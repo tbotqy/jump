6b5b6dab0b70c8b6b45e8077b8037d0b9c6448a4
tbotaq <tbotaq@gmail.com>
2019-06-16 15:43:02 +0900

    implement callback for 'Sign in with Twitter' OAuth

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,68 @@
+# frozen_string_literal: true
+
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def twitter
+    user = User.register_or_update!(user_params)
+    sign_in user
+
+    if user.finished_initial_import?
+      redirect_to user_timeline_path
+    else
+      redirect_to status_import_path
+    end
+  rescue => e
+    # on the development mode,
+    Raven.capture_exception(e)
+    head 500
+ end
+
+  # this methods is called when the user declined to authorize our app
+  def failure
+    redirect_to sign_out_path
+  end
+
+  private
+
+    delegate :provider,
+             :uid,
+             to: :auth
+
+    delegate :created_at,
+             :name,
+             :screen_name,
+             :protected,
+             :profile_image_url_https,
+             to: :auth_raw_info
+
+    delegate :token,
+             :secret,
+             to: :auth_credentials
+
+    def user_params
+      {
+        provider:           provider,
+        uid:                uid,
+        twitter_id:         uid.to_i,
+        twitter_created_at: created_at,
+        name:               name,
+        screen_name:        screen_name,
+        protected:          protected,
+        profile_image_url_https: profile_image_url_https,
+        token:              token,
+        token_secret:       secret
+      }
+    end
+
+    def auth
+      @auth ||= request.env["omniauth.auth"]
+    end
+
+    def auth_raw_info
+      @auth_raw_info ||= auth.extra.raw_info
+    end
+
+    # memo: not using #delegate in order to unify the level of delegations in this class
+    def auth_credentials
+      @auth_credentials ||= auth.credentials
+    end
+end
