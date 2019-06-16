17a569de1d6621a61cef718a96c26c3f73a17701
tbotaq <tbotaq@gmail.com>
2019-06-16 15:46:09 +0900

    prepare sign_out action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  # including this because ActionController::API doesn't implement #respond_to
+  # , which is required for Devise::SessionsController#sign_out to work without raising NoMethodError.
+  # TODO: implement sign_out without using ::API controller
+  include ActionController::MimeResponds
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
+  devise_scope :user do
+    get "sign_out", to: "devise/sessions#destroy"
+  end
+
   direct :user_timeline do
     "/user_timeline"
   end
