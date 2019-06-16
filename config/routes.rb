72f0f3717a317ca260d7269c29e68c3990bbcfec
tbotaq <tbotaq@gmail.com>
2019-06-16 10:15:44 +0900

    add routing to use OAuth

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 end
