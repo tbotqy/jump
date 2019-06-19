de4fcb0358ec62693ce51d7b302d67977d27f43a
tbotaq <tbotaq@gmail.com>
2019-06-19 15:21:01 +0900

    implement error handlers and action to authenticate user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,40 @@ class ApplicationController < ActionController::API
   # , which is required for Devise::SessionsController#sign_out to work without raising NoMethodError.
   # TODO: implement sign_out without using ::API controller
   include ActionController::MimeResponds
+
+  rescue_from RuntimeError,                 with: :render_500
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+
+  rescue_from Errors::BadRequest,           with: :render_400
+  rescue_from Errors::Unauthorized,         with: :render_401
+
+  private
+
+    def authenticate_user!
+      raise Errors::Unauthorized, "The request need authentication." unless user_signed_in?
+    end
+
+    def render_400(e)
+      render_error(e, 400)
+    end
+
+    def render_401(e)
+      render_error(e, 401)
+    end
+
+    def render_404(e)
+      render_error(e, 404)
+    end
+
+    def render_500(e)
+      render_error(e, 500)
+    end
+
+    def render_error(e, status_code)
+      Rails.logger.error(e.message)
+      Raven.capture_exception(e)
+
+      messages = e.try(:messages) || Array.wrap(e.message)
+      render json: { messages: messages }, status: status_code
+    end
 end
