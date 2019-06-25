3e2274e20646f4b519a88021d4d0c53a740e0eae
tbotaq <tbotaq@gmail.com>
2019-06-25 17:54:08 +0900

    implement and specify CollectUserStatusesService

---
+++ app/services/collect_user_statuses_service/date_parser.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+class CollectUserStatusesService
+  class DateParser
+    def initialize(year, month, day)
+      @year  = year
+      @month = month
+      @day   = day
+    end
+
+    def date_specified?
+      [year, month, day].any?(&:present?)
+    end
+
+    def last_moment_of_params!
+      if [year, month, day].all?(&:present?)
+        # (2019, 3, 10).end_of_day
+        Time.local(year, month, day).end_of_day
+      elsif [year, month].all?(&:present?)
+        # (2019, 3).end_of_month
+        Time.local(year, month).end_of_month
+      elsif year.present? && day.blank?
+        # 2019.end_of_year
+        Time.local(year).end_of_year
+      else
+        raise Errors::BadRequest, "Given date(year: #{year}, month: #{month}, day: #{day}) is incomplete to be parsed to Time."
+      end
+    end
+
+    private
+      attr_reader :year, :month, :day
+  end
+end
