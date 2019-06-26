51accc802f8f80ceafee17d4a16a4fb4e3fa0026
tbotaq <tbotaq@gmail.com>
2019-06-26 14:43:42 +0900

    apply DRY for examples

--- spec/services/collect_user_statuses_service/date_parser_spec.rb
+++ spec/services/collect_user_statuses_service/date_parser_spec.rb
@@ -4,7 +4,7 @@ require "rails_helper"
 
 def presence_of(obj)
   return "nil " if obj.nil?
-  return "''"   if obj == ""
+  return "blank string" if obj == ""
   obj
 end
 
@@ -78,27 +78,33 @@ describe CollectUserStatusesService::DateParser do
       it { expect { subject }.to raise_error(Errors::InvalidParam, "Given date(year: #{year}, month: #{month}, day: #{day}) is incomplete to be parsed to Time.") }
     end
 
+    shared_examples "returns end of given day" do
+      it { is_expected.to eq Time.local(year, month, day).end_of_day }
+    end
+
+    shared_examples "returns end of given month" do
+      it { is_expected.to eq Time.local(year, month).end_of_month }
+    end
+
+    shared_examples "returns end of given year" do
+      it { is_expected.to eq Time.local(year).end_of_year }
+    end
+
     context "year: 2019" do
       let(:year) { 2019 }
       context "month: 3" do
         let(:month) { 3 }
         context "day: 7" do
           let(:day) { 7 }
-          it "returns end of given day" do
-            is_expected.to eq Time.local(2019, 3, 7).end_of_day
-          end
+          it_behaves_like "returns end of given day"
         end
         context "day: nil" do
           let(:day) { nil }
-          it "returns end of given month" do
-            is_expected.to eq Time.local(2019, 3).end_of_month
-          end
+          it_behaves_like "returns end of given month"
         end
-        context "day: ''" do
+        context "day: blank string" do
           let(:day) { "" }
-          it "returns end of given month" do
-            is_expected.to eq Time.local(2019, 3).end_of_month
-          end
+          it_behaves_like "returns end of given month"
         end
       end
       context "month: nil" do
@@ -109,18 +115,14 @@ describe CollectUserStatusesService::DateParser do
         end
         context "day: nil" do
           let(:day) { nil }
-          it "returns end of given year" do
-            is_expected.to eq Time.local(2019).end_of_year
-          end
+          it_behaves_like "returns end of given year"
         end
-        context "day: ''" do
+        context "day: blank string" do
           let(:day) { "" }
-          it "returns end of given year" do
-            is_expected.to eq Time.local(2019).end_of_year
-          end
+          it_behaves_like "returns end of given year"
         end
       end
-      context "month: ''" do
+      context "month: blank string" do
         let(:month) { "" }
         context "day: 7" do
           let(:day) { 7 }
@@ -128,27 +130,23 @@ describe CollectUserStatusesService::DateParser do
         end
         context "day: nil" do
           let(:day) { nil }
-          it "returns end of given year" do
-            is_expected.to eq Time.local(2019).end_of_year
-          end
+          it_behaves_like "returns end of given year"
         end
-        context "day: ''" do
+        context "day: blank string" do
           let(:day) { "" }
-          it "returns end of given year" do
-            is_expected.to eq Time.local(2019).end_of_year
-          end
+          it_behaves_like "returns end of given year"
         end
       end
     end
 
     [nil, ""].each do |year|
       context "year: #{presence_of(year)}" do
-        let(:year) { year }
         [3, nil, ""].each do |month|
           context "month: #{presence_of(month)}" do
-            let(:month) { month }
             [7, nil, ""].each do |day|
               context "day: #{presence_of(day)}" do
+                let(:year) { year }
+                let(:month) { month }
                 let(:day) { day }
                 it_behaves_like "raises error"
               end
