0b23805ee470939d1d9e1620358f5446f7159326
tbotaq <tbotaq@gmail.com>
2019-06-28 18:32:13 +0900

    prepend the name of app to namespace of sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://#{ENV.fetch('JUMP_CACHE_HOST')}:#{ENV.fetch('JUMP_CACHE_PORT')}", namespace: "sidekiq_#{Rails.env}" }
+  config.redis = { url: "redis://#{ENV.fetch('JUMP_CACHE_HOST')}:#{ENV.fetch('JUMP_CACHE_PORT')}", namespace: "jump_sidekiq_#{Rails.env}" }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://#{ENV.fetch('JUMP_CACHE_HOST')}:#{ENV.fetch('JUMP_CACHE_PORT')}", namespace: "sidekiq_#{Rails.env}" }
+  config.redis = { url: "redis://#{ENV.fetch('JUMP_CACHE_HOST')}:#{ENV.fetch('JUMP_CACHE_PORT')}", namespace: "jump_sidekiq_#{Rails.env}" }
 end
 
 #schedule_file = "config/sidekiq_cron_schedule.yml"
