82b96ca3d408555db011e0fe68196c23dd895f74
tbotaq <tbotaq@gmail.com>
2019-07-03 00:09:59 +0900

    refactor service class to fetch user tweets via api

---
+++ app/services/fetch_user_tweets_service.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+class FetchUserTweetsService
+  private_class_method :new
+
+  MAX_TWEETS_COUNT_PER_GET = 200
+  private_constant :MAX_TWEETS_COUNT_PER_GET
+
+  class << self
+    def call!(user_id:, tweeted_after_id:, tweeted_before_id: nil)
+      new(user_id, tweeted_after_id, tweeted_before_id).send(:call!)
+    end
+  end
+
+  private
+    attr_reader :user_id, :tweeted_after_id, :tweeted_before_id
+
+    def initialize(user_id, tweeted_after_id, tweeted_before_id)
+      @user_id           = user_id
+      @tweeted_after_id  = tweeted_after_id
+      @tweeted_before_id = tweeted_before_id
+    end
+
+    def call!
+      twitter_rest_client.user_timeline(twitter_id, api_params)
+    end
+
+    def api_params
+      max_id = tweeted_before_id.present? ? tweeted_before_id - 1 : nil
+      default_api_params
+        .merge(since_id: tweeted_after_id)
+        .merge(max_id:   max_id)
+        .compact
+    end
+
+    def default_api_params
+      {
+        include_rts: true,
+        count:       MAX_TWEETS_COUNT_PER_GET
+      }
+    end
+
+    def twitter_id
+      @twitter_id ||= User.find(user_id).twitter_id
+    end
+
+    def twitter_rest_client
+      TwitterRestClient.by_user_id(user_id)
+    end
+end
