52158aa3664b5270d51f0f50e5d5391fce5033c6
tbotaq <tbotaq@gmail.com>
2019-07-03 00:28:17 +0900

    tweak

--- app/jobs/tweet_import_job.rb
+++ app/jobs/tweet_import_job.rb
@@ -5,41 +5,39 @@ class TweetImportJob < ApplicationJob
 
   def perform(user_id:)
     @user_id = user_id
-    determine_smallest_tweet_id_to_fetch!
-    process!
+    initialize_parameter
+    import!
   end
 
   private
-    def determine_smallest_tweet_id_to_fetch!
-      # MEMO : this method is intended to be called under these conditions
-      # - only once
-      # - before #process! is called
+    attr_reader :user_id
 
-      # raise if this method seems to be called in several times
-      raise "This method is intended to be called only once." if defined?(@smallest_tweet_id_to_fetch)
+    def initialize_parameter
       if user.has_any_status?
-        @smallest_tweet_id_to_fetch = user.status_newest_in_tweeted_time.status_id_str + 1
+        @initial_most_recent_tweet_id = user.status_newest_in_tweeted_time.status_id_str
       else
-        @smallest_tweet_id_to_fetch = nil
+        @initial_most_recent_tweet_id = nil
       end
     end
 
-    def process!
-      largest_tweet_id_to_fetch = nil
+    def import!
+      oldest_id_of_all_fetched_tweets = nil
       loop do
-        tweets = FetchUserTweetsByIdRangeService.call!(user_id: @user_id, min_tweet_id: @smallest_tweet_id_to_fetch, max_tweet_id: largest_tweet_id_to_fetch)
+        tweets = FetchUserTweetsService.call!(user_id: user_id, tweeted_after_id: @initial_most_recent_tweet_id, tweeted_before_id: oldest_id_of_all_fetched_tweets)
         break if tweets.blank?
         save_tweets!(tweets)
         record_progress!(tweets.count)
+
         # specify for subsequent fetch
-        largest_tweet_id_to_fetch = tweets.last.id - 1
+        # oldest_id_of_all_fetched_tweets gets smaller
+        oldest_id_of_all_fetched_tweets = tweets.last.id
       end
       close_progress!
       update_summary
     end
 
     def save_tweets!(tweets)
-      Status.save_tweets!(@user_id, tweets)
+      Status.save_tweets!(user_id, tweets)
     end
 
     def record_progress!(tweet_count_additionally_imported)
@@ -55,14 +53,14 @@ class TweetImportJob < ApplicationJob
     end
 
     def job_progress
-      @job_progress ||= TweetImportJobProgress.new(job_id: job_id, user_id: @user_id, percentage_denominator: estimated_number_of_tweets_to_be_imported)
+      @job_progress ||= TweetImportJobProgress.new(job_id: job_id, user_id: user_id, percentage_denominator: estimated_number_of_tweets_to_be_imported)
     end
 
     def user
-      @user ||= User.find(@user_id)
+      @user ||= User.find(user_id)
     end
 
     def estimated_number_of_tweets_to_be_imported
-      CalculateAcquirableTweetCountService.call!(user_id: @user_id)
+      CalculateAcquirableTweetCountService.call!(user_id: user_id)
     end
 end
