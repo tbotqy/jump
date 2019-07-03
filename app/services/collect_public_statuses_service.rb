1b230d78c76e1121536423e2ced5dd70d92e05fb
tbotaq <tbotaq@gmail.com>
2019-07-04 08:42:01 +0900

    make methods private

--- app/services/collect_public_statuses_service.rb
+++ app/services/collect_public_statuses_service.rb
@@ -1,31 +1,34 @@
 # frozen_string_literal: true
 
 class CollectPublicStatusesService
+  private_class_method :new
+
   class << self
     def call!(year: nil, month: nil, day: nil, page: 1)
-      new(year, month, day, page).call!
+      new(year, month, day, page).send(:call!)
     end
-  end
-
-  def initialize(year, month, day, page)
-    @year     = year
-    @month    = month
-    @day      = day
-    @page     = page
-    @collection = []
-  end
-
-  def call!
-    fetch_public_statuses_all
-    scope_by_date if date_specified?
-    apply_pagination
-    check_if_collection_exists!
-    @collection.order_for_timeline
   end
 
   private
     include DateParsable
+
+    def initialize(year, month, day, page)
+      @year     = year
+      @month    = month
+      @day      = day
+      @page     = page
+      @collection = []
+    end
+
     attr_reader :year, :month, :day, :page
+
+    def call!
+      fetch_public_statuses_all
+      scope_by_date if date_specified?
+      apply_pagination
+      check_if_collection_exists!
+      @collection.order_for_timeline
+    end
 
     def fetch_public_statuses_all
       @collection = Status.not_private.includes(:entities)
