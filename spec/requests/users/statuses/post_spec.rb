01fbbf4dca45ee94d850e24d933814923a454c5f
tbotaq <tbotaq@gmail.com>
2019-07-08 03:27:45 +0900

    stop testing routing layer

--- spec/requests/users/statuses/post_spec.rb
+++ spec/requests/users/statuses/post_spec.rb
@@ -8,62 +8,56 @@ RSpec.describe "Users::Statuses", type: :request do
     shared_examples "doesn't enqueue the job" do
       it { expect(ImportTweetsJob).not_to have_been_enqueued }
     end
-    context "id is not given" do
+
+    context "user has not been authenticated" do
       let!(:user_id) { create(:user).id }
+      before { subject }
       it_behaves_like "doesn't enqueue the job"
-      it { expect { subject }.to raise_error }
+      it_behaves_like "respond with status code", :unauthorized
     end
-    context "id is given" do
-      context "user has not been authenticated" do
-        let!(:user_id) { create(:user).id }
-        before { subject }
+    context "user has been authenticated" do
+      context "user with given id doesn't exist" do
+        let!(:user)   { create(:user) }
+        let(:user_id) { User.maximum(:id) + 1 }
+        before do
+          sign_in user
+          subject
+        end
         it_behaves_like "doesn't enqueue the job"
-        it_behaves_like "respond with status code", :unauthorized
+        it_behaves_like "respond with status code", :not_found
       end
-      context "user has been authenticated" do
-        context "user with given id doesn't exist" do
-          let!(:user)   { create(:user) }
-          let(:user_id) { User.maximum(:id) + 1 }
+      context "user with given id exists" do
+        context "given id isn't an authenticated user's id" do
+          let!(:user)    { create(:user) }
+          let!(:user_id) { create(:user).id }
           before do
             sign_in user
             subject
           end
           it_behaves_like "doesn't enqueue the job"
-          it_behaves_like "respond with status code", :not_found
+          it_behaves_like "request for the other user's resource"
         end
-        context "user with given id exists" do
-          context "given id isn't an authenticated user's id" do
-            let!(:user)    { create(:user) }
-            let!(:user_id) { create(:user).id }
+        context "given id is an authenticated user's id" do
+          let!(:user)   { create(:user) }
+          let(:user_id) { user.id }
+          context "failed to kick the job" do
             before do
+              allow(ImportTweetsJob).to receive(:perform_later).and_raise(RuntimeError)
               sign_in user
               subject
             end
             it_behaves_like "doesn't enqueue the job"
-            it_behaves_like "request for the other user's resource"
+            it_behaves_like "respond with status code", :internal_server_error
           end
-          context "given id is an authenticated user's id" do
-            let!(:user)   { create(:user) }
-            let(:user_id) { user.id }
-            context "failed to kick the job" do
-              before do
-                allow(ImportTweetsJob).to receive(:perform_later).and_raise(RuntimeError)
-                sign_in user
-                subject
-              end
-              it_behaves_like "doesn't enqueue the job"
-              it_behaves_like "respond with status code", :internal_server_error
+          context "succeeded to kick the job" do
+            before do
+              sign_in user
+              subject
             end
-            context "succeeded to kick the job" do
-              before do
-                sign_in user
-                subject
-              end
-              it "enqueues the job " do
-                expect(ImportTweetsJob).to have_been_enqueued.exactly(:once)
-              end
-              it_behaves_like "respond with status code", :accepted
+            it "enqueues the job " do
+              expect(ImportTweetsJob).to have_been_enqueued.exactly(:once)
             end
+            it_behaves_like "respond with status code", :accepted
           end
         end
       end
