4d2b8cb6ce55fb91210317bab1bfd3d8c29c441e
tbotaq <tbotaq@gmail.com>
2019-07-09 13:31:12 +0900

    delete unused implementation

--- app/models/followee.rb
+++ app/models/followee.rb
@@ -2,18 +2,4 @@
 
 class Followee < ApplicationRecord
   belongs_to :user
-  scope :user_id, ->(user_id) { where(user_id: user_id) }
-
-  class << self
-    def register!(user_id, twitter_ids)
-      Array.wrap(twitter_ids).each do |twitter_id|
-        create!(
-          user_id: user_id,
-          twitter_id: twitter_id
-        )
-      end
-
-      User.find(user_id).update_attribute(:friends_updated_at, Time.now)
-    end
-  end
 end
