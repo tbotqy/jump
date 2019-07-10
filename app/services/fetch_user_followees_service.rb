5f0d1c7fb64fece5995ee911621caa480a802b8a
tbotaq <tbotaq@gmail.com>
2019-07-10 12:27:23 +0900

    remove the useless use of instance var

--- app/services/fetch_user_followees_service.rb
+++ app/services/fetch_user_followees_service.rb
@@ -11,24 +11,24 @@ class FetchUserFolloweesService
 
   private
     def initialize(user_id)
-      @user_id              = user_id
-      @next_cursor          = -1
-      @followee_twitter_ids = []
+      @user_id     = user_id
+      @next_cursor = -1
     end
 
     attr_reader :user_id
 
     def call!
       fetch_followee_twitter_ids_all!
-      @followee_twitter_ids
     end
 
     def fetch_followee_twitter_ids_all!
+      followee_twitter_ids = []
       while !@next_cursor.zero?
         followees              = twitter_rest_client.friend_ids(cursor: @next_cursor)
-        @followee_twitter_ids += followees.attrs[:ids]
+        followee_twitter_ids  += followees.attrs[:ids]
         @next_cursor           = followees.attrs[:next_cursor]
       end
+      followee_twitter_ids
     end
 
     def twitter_rest_client
