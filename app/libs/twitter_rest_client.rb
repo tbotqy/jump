feb803e1f74229a0c59f34c337a61d1039e26da1
tbotaq <tbotaq@gmail.com>
2019-07-11 14:54:30 +0900

    rename consumer_secret_key to consumer_secret

--- app/libs/twitter_rest_client.rb
+++ app/libs/twitter_rest_client.rb
@@ -6,7 +6,7 @@ class TwitterRestClient
       user = User.find(user_id)
       Twitter::REST::Client.new do |config|
         config.consumer_key        = Settings.twitter.consumer_key
-        config.consumer_secret     = Settings.twitter.consumer_secret_key
+        config.consumer_secret     = Settings.twitter.consumer_secret
         config.access_token        = user.access_token
         config.access_token_secret = user.access_token_secret
       end
