fbb2bb28be36c612a62b4d5843359b5edaab5508
tbotaq <tbotaq@gmail.com>
2019-07-19 09:46:16 +0900

    Remove ore-ore creation.

--- app/jobs/profile_update_job.rb
+++ app/jobs/profile_update_job.rb
@@ -8,7 +8,7 @@ class ProfileUpdateJob < ApplicationJob
       UpdateUserAccountService.call!(user_id: user_id)
     rescue Twitter::Error => twitter_error
       # TODO: include to service class
-      ProfileUpdateFailLog.log!(user_id, twitter_error.message)
+      ProfileUpdateFailLog.create!(user_id: user_id, error_message: twitter_error.message)
       next
     end
   end
