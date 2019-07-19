85cc8521fc523271dd7039be9e91f32aba6c13da
tbotaq <tbotaq@gmail.com>
2019-07-19 11:04:12 +0900

    Specify validations on Url.

---
+++ spec/models/url_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Url, type: :model do
+  describe "validations" do
+    describe "#url" do
+      it { should validate_presence_of(:url) }
+      it { should validate_length_of(:url).is_at_most(255) }
+    end
+    describe "#display_url" do
+      it { should validate_presence_of(:display_url) }
+      it { should validate_length_of(:display_url).is_at_most(255) }
+    end
+    include_examples "validation on indices"
+  end
+end
