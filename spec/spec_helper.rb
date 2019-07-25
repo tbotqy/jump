95acd3a224e713b34eb954e99c33ba2ed7690e48
tbotaq <tbotaq@gmail.com>
2019-07-25 16:10:29 +0900

    Clean with truncation to make test even faster.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -113,6 +113,7 @@ RSpec.configure do |config|
 
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
   end
 
   config.before(:each) do
