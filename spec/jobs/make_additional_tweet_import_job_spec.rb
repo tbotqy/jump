cfe62f29f24acdbb1c0c72b8f04747dde05550ed
tbotaq <tbotaq@gmail.com>
2019-07-29 22:15:35 +0900

    Modify service class to handle with job lock.

--- spec/jobs/make_additional_tweet_import_job_spec.rb
+++ spec/jobs/make_additional_tweet_import_job_spec.rb
@@ -21,6 +21,24 @@ RSpec.describe MakeAdditionalTweetImportJob, type: :job do
     end
   end
 
+  shared_examples "the job lock is once acquired" do
+    before do
+      allow(user).to receive(:create_tweet_import_lock!).and_call_original
+      allow(User).to receive(:find).and_return(user)
+    end
+    it do
+      subject.call rescue nil
+      expect(user).to have_received(:create_tweet_import_lock!).once
+    end
+  end
+
+  shared_examples "doesn't leave the job locked" do
+    it do
+      subject.call rescue nil
+      expect(TweetImportLock.exists?(user_id: user_id)).to eq false
+    end
+  end
+
   shared_context "user has 2 registered tweets and 10 unregistered tweets" do
     let(:user)    { create(:user, statuses_updated_at: 10) }
     let(:user_id) { user.id }
@@ -49,76 +67,103 @@ RSpec.describe MakeAdditionalTweetImportJob, type: :job do
     it { is_expected.to raise_error(ActiveRecord::RecordNotFound) }
   end
   context "succeeds to identify the user" do
-    context "user has no status yet" do
-      let(:user)    { create(:user) }
+    context "the job is locked " do
+      let!(:user)   { create(:user) }
       let(:user_id) { user.id }
-      it { is_expected.to raise_error("This job is intended to be called for additional import, but user has no status.") }
+      before { create(:tweet_import_lock, user: user) }
+      it { is_expected.to raise_error("The job has been locked.") }
       it_behaves_like "makes no change"
-    end
-    context "user has some statuses" do
-      context "fails to fetch the most recent tweet id of user's statuses" do
-        let(:user)    { create(:user) }
-        let(:user_id) { user.id }
-        before do
-          create_list(:status, 2, user: user)
-          allow(Status).to receive(:most_recent_tweet_id!).and_raise(ActiveRecord::RecordNotFound)
-        end
-        it { is_expected.to raise_error(ActiveRecord::RecordNotFound) }
-        it_behaves_like "makes no change"
+      it "keeps the job locked" do
+        subject.call rescue nil
+        expect(TweetImportLock.exists?(user_id: user_id)).to eq true
       end
-      context "succeeds to fetch the most recent tweet id of user's statuses" do
-        context "fails to fetch tweets via api" do
+    end
+    context "the job is not locked" do
+      context "user has no status yet" do
+        let!(:user)   { create(:user) }
+        let(:user_id) { user.id }
+        it { is_expected.to raise_error("This job is intended to be called for additional import, but user has no status.") }
+        it_behaves_like "makes no change"
+        it_behaves_like "the job lock is once acquired"
+        it_behaves_like "doesn't leave the job locked"
+      end
+      context "user has some statuses" do
+        context "fails to fetch the most recent tweet id of user's statuses" do
           let(:user)    { create(:user) }
           let(:user_id) { user.id }
           before do
-            registered_tweet_ids = create_list(:status, 2, user: user).pluck(:tweet_id)
-            most_recent_tweet_id = registered_tweet_ids.max
-            allow_any_instance_of(Twitter::REST::Client).to receive(:user_timeline).with(hash_including(since_id: most_recent_tweet_id)).and_raise(Twitter::Error)
+            create_list(:status, 2, user: user)
+            allow(Status).to receive(:most_recent_tweet_id!).and_raise(ActiveRecord::RecordNotFound)
           end
-          it { is_expected.to raise_error(Twitter::Error) }
+          it { is_expected.to raise_error(ActiveRecord::RecordNotFound) }
           it_behaves_like "makes no change"
+          it_behaves_like "the job lock is once acquired"
+          it_behaves_like "doesn't leave the job locked"
         end
-        context "succeeds to fetch tweets via api" do
-          context "fails to register fetched tweets" do
-            include_context "user has 2 registered tweets and 10 unregistered tweets"
-            before { allow(RegisterTweetService).to receive(:call!).and_raise(ActiveRecord::RecordInvalid) }
-            it { is_expected.to raise_error(ActiveRecord::RecordInvalid) }
+        context "succeeds to fetch the most recent tweet id of user's statuses" do
+          context "fails to fetch tweets via api" do
+            let(:user)    { create(:user) }
+            let(:user_id) { user.id }
+            before do
+              registered_tweet_ids = create_list(:status, 2, user: user).pluck(:tweet_id)
+              most_recent_tweet_id = registered_tweet_ids.max
+              allow_any_instance_of(Twitter::REST::Client).to receive(:user_timeline).with(hash_including(since_id: most_recent_tweet_id)).and_raise(Twitter::Error)
+            end
+            it { is_expected.to raise_error(Twitter::Error) }
             it_behaves_like "makes no change"
+            it_behaves_like "the job lock is once acquired"
+            it_behaves_like "doesn't leave the job locked"
           end
-          context "succeeds to register fetched tweets" do
-            context "fails to update timestamp" do
+          context "succeeds to fetch tweets via api" do
+            context "fails to register fetched tweets" do
               include_context "user has 2 registered tweets and 10 unregistered tweets"
-              before do
-                travel_to(Time.now.utc)
-                allow_any_instance_of(User).to receive(:update!).with(statuses_updated_at: Time.now.utc.to_i).and_raise(ActiveRecord::RecordInvalid)
-              end
-              after { travel_back }
+              before { allow(RegisterTweetService).to receive(:call!).and_raise(ActiveRecord::RecordInvalid) }
               it { is_expected.to raise_error(ActiveRecord::RecordInvalid) }
               it_behaves_like "makes no change"
+              it_behaves_like "the job lock is once acquired"
+              it_behaves_like "doesn't leave the job locked"
             end
-            context "succeeds to update timestamp" do
-              context "fails to increment ActiveStatusCount" do
+            context "succeeds to register fetched tweets" do
+              context "fails to update timestamp" do
                 include_context "user has 2 registered tweets and 10 unregistered tweets"
-                before { allow(ActiveStatusCount).to receive(:increment_by).and_raise(Redis::CannotConnectError) }
-                it { is_expected.to raise_error(Redis::CannotConnectError) }
+                before do
+                  travel_to(Time.now.utc)
+                  allow_any_instance_of(User).to receive(:update!).with(statuses_updated_at: Time.now.utc.to_i).and_raise(ActiveRecord::RecordInvalid)
+                end
+                after { travel_back }
+                it { is_expected.to raise_error(ActiveRecord::RecordInvalid) }
                 it_behaves_like "makes no change"
+                it_behaves_like "the job lock is once acquired"
+                it_behaves_like "doesn't leave the job locked"
               end
-              context "succeeds to increment ActiveStatusCount" do
-                include_context "user has 2 registered tweets and 10 unregistered tweets"
-                it { is_expected.not_to raise_error }
-                describe "registers tweets" do
-                  it { is_expected.to change { User.find(user_id).statuses.count }.by(unregistered_tweets_count) }
-                  it { is_expected.to change { User.find(user_id).hashtags.count } }
-                  it { is_expected.to change { User.find(user_id).urls.count } }
-                  it { is_expected.to change { User.find(user_id).media.count } }
+              context "succeeds to update timestamp" do
+                context "fails to increment ActiveStatusCount" do
+                  include_context "user has 2 registered tweets and 10 unregistered tweets"
+                  before { allow(ActiveStatusCount).to receive(:increment_by).and_raise(Redis::CannotConnectError) }
+                  it { is_expected.to raise_error(Redis::CannotConnectError) }
+                  it_behaves_like "makes no change"
+                  it_behaves_like "the job lock is once acquired"
+                  it_behaves_like "doesn't leave the job locked"
                 end
-                it "increments ActiveStatusCount by the number of tweets" do
-                  is_expected.to change { ActiveStatusCount.current_count }.by(unregistered_tweets_count)
-                end
-                describe "updates timestamp" do
-                  before { travel_to(Time.now.utc) }
-                  after  { travel_back }
-                  it { is_expected.to change { User.find(user_id).statuses_updated_at }.to(Time.now.utc.to_i) }
+                context "succeeds to increment ActiveStatusCount" do
+                  include_context "user has 2 registered tweets and 10 unregistered tweets"
+                  it { is_expected.not_to raise_error }
+                  describe "registers tweets" do
+                    it { is_expected.to change { User.find(user_id).statuses.count }.by(unregistered_tweets_count) }
+                    it { is_expected.to change { User.find(user_id).hashtags.count } }
+                    it { is_expected.to change { User.find(user_id).urls.count } }
+                    it { is_expected.to change { User.find(user_id).media.count } }
+                  end
+                  it "increments ActiveStatusCount by the number of tweets" do
+                    is_expected.to change { ActiveStatusCount.current_count }.by(unregistered_tweets_count)
+                  end
+                  describe "updates timestamp" do
+                    before { travel_to(Time.now.utc) }
+                    after  { travel_back }
+                    it { is_expected.to change { User.find(user_id).statuses_updated_at }.to(Time.now.utc.to_i) }
+                  end
+                  it_behaves_like "the job lock is once acquired"
+                  it_behaves_like "doesn't leave the job locked"
                 end
               end
             end
