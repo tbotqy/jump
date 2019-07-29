9f05e47c95d9da539b17c7adff1849bcf3ce21f4
tbotaq <tbotaq@gmail.com>
2019-07-29 22:31:41 +0900

    Implement and specify the request: PUT /users/:id/statuses

--- app/controllers/users/statuses_controller.rb
+++ app/controllers/users/statuses_controller.rb
@@ -24,6 +24,19 @@ module Users
       end
     end
 
+    # PUT /users/:id/statuses
+    def update
+      user = User.find(params[:user_id])
+      authorize_operation_for!(user)
+
+      if user.tweet_import_lock.present?
+        head :too_many_requests
+      else
+        MakeAdditionalTweetImportJob.perform_later(user_id: params[:user_id])
+        head :accepted
+      end
+    end
+
     private
       def params_to_collect_by
         params.permit(:user_id, :year, :month, :day, :page).to_h.symbolize_keys
