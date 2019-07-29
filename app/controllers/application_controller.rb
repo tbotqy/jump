b142777bc2f933e94d90779450a74dcbeb54c72f
tbotaq <tbotaq@gmail.com>
2019-07-29 17:08:46 +0900

    Change the inheritance of Devise.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
-  # including this because ActionController::API doesn't implement #respond_to
-  # , which is required for Devise::SessionsController#sign_out to work without raising NoMethodError.
-  # TODO: implement sign_out without using ::API controller
-  include ActionController::MimeResponds
-
   rescue_from RuntimeError,                 with: :render_500
   rescue_from ActiveRecord::RecordNotFound, with: :render_404
 
