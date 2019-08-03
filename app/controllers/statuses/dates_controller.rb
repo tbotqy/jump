4594d31486da08a6d8e6aafd02f091832fe106e4
tbotaq <tbotaq@gmail.com>
2019-08-03 15:53:10 +0900

    Implement and specify GET /statuses/dates

--- app/controllers/statuses/dates_controller.rb
+++ app/controllers/statuses/dates_controller.rb
@@ -3,6 +3,10 @@
 module Statuses
   class DatesController < ApplicationController
     def index
+      tweeted_dates = StatusesDecorator.new(Status.not_private).tweeted_dates
+      raise Errors::NotFound if tweeted_dates.blank?
+
+      render json: tweeted_dates
     end
   end
 end
