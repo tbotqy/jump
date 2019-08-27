377fc9bfccb4654b11338ac48242b598944ab723
tbotaq <tbotaq@gmail.com>
2019-08-27 12:01:58 +0900

    Add lacked importing.

--- frontend/src/containers/UserTimelineContainer.js
+++ frontend/src/containers/UserTimelineContainer.js
@@ -8,6 +8,7 @@ import {
   setSelectableDates,
   fetchUserSelectableDates
 } from "../actions/selectableDatesActions";
+import { setApiErrorCode } from "../actions/apiErrorActions";
 import UserTimeline from "../components/UserTimeline";
 
 const mapDispatchToProps = dispatch => ({
@@ -15,7 +16,8 @@ const mapDispatchToProps = dispatch => ({
   setIsFetching:            flag => dispatch(setIsFetching(flag)),
   setSelectableDates:       dates => dispatch(setSelectableDates(dates)),
   fetchUserTweets:          (year, month, day, page) => dispatch(fetchUserTweets(year, month, day, page)),
-  fetchUserSelectableDates: () => dispatch(fetchUserSelectableDates())
+  fetchUserSelectableDates: () => dispatch(fetchUserSelectableDates()),
+  setApiErrorCode:          code => dispatch(setApiErrorCode(code))
 });
 
 export default connect(null, mapDispatchToProps)(UserTimeline);
