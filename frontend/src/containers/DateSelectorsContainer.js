5516e2e2d788b159445ce1abcb98446242ae80cc
tbotaq <tbotaq@gmail.com>
2019-09-01 22:15:13 +0900

    Show the progress bar when a selected date changed.

--- frontend/src/containers/DateSelectorsContainer.js
+++ frontend/src/containers/DateSelectorsContainer.js
@@ -1,6 +1,7 @@
 import { connect } from "react-redux";
 import {
   setTweets,
+  setIsFetching,
   resetHasMore
 } from "../actions/tweetsActions";
 import { resetPage } from "../actions/pageActions";
@@ -8,6 +9,7 @@ import DateSelectors from "../components/timeline/DateSelectors";
 
 const mapDispatchToProps = dispatch => ({
   setTweets: tweets => dispatch(setTweets(tweets)),
+  setIsFetching: flag => dispatch(setIsFetching(flag)),
   resetPage: () => dispatch(resetPage()),
   resetHasMore: () => dispatch(resetHasMore())
 });
