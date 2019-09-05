ab2f2baadc0a2fddf3e65467f19934d0e3b15ba6
tbotaq <tbotaq@gmail.com>
2019-09-05 22:21:52 +0900

    Refresh the stored user data every time the user visits DataManagement page.

--- frontend/src/components/DataManagement.jsx
+++ frontend/src/components/DataManagement.jsx
@@ -36,62 +36,69 @@ const styles = theme => ({
   }
 });
 
-const DataManagement = props => {
-  const { user, classes } = props;
+class DataManagement extends React.Component {
+  componentDidMount() {
+    this.props.fetchUser()
+      .then( response => this.props.setUser(response.data) )
+      .catch( error => this.props.setApiErrorCode(error.response.status) );
+  }
 
-  const requestTweetImport = () => {
+  render() {
+    const { user, classes } = this.props;
+    return (
+      <React.Fragment>
+        <HeadNav />
+        <Container className={ classes.container }>
+          <ApiErrorBoundary>
+            <Typography variant="h4" className={ classes.typography }>
+              データ管理
+            </Typography>
+            {
+              user ? (
+                <>
+                  <List>
+                    <CustomizedListItem
+                      icon={ <TextsmsIcon /> }
+                      headerText="ツイート"
+                      numberText={ `${user.status_count} 件` }
+                      updatedAt={ user.statuses_updated_at ? formatDateString(user.statuses_updated_at) : "-" }
+                      apiFunc={ this.requestTweetImport }
+                    />
+                    <CustomizedListItem
+                      icon={ <PeopleIcon /> }
+                      headerText="フォローリスト"
+                      numberText={ `${user.followee_count} 件` }
+                      updatedAt={ user.followees_updated_at ? formatDateString(user.followees_updated_at) : "-" }
+                      apiFunc={ this.requestFolloweeImport }
+                    />
+                  </List>
+                  <Divider />
+                  <List>
+                    <ListItem className={ classes.deleteButtonListItem }>
+                      <ListItemSecondaryAction>
+                        <AccountDeleteDialog />
+                      </ListItemSecondaryAction>
+                    </ListItem>
+                  </List>
+                </>
+              ): <CircularProgress />
+            }
+          </ApiErrorBoundary>
+        </Container>
+        <Footer bgCaramel />
+      </React.Fragment>
+    );
+  }
+
+  requestTweetImport() {
     const userId = getUserIdFromCookie();
     return api.put(`/users/${userId}/statuses`);
-  };
+  }
 
-  const requestFolloweeImport = () => {
+  requestFolloweeImport() {
     const userId = getUserIdFromCookie();
     return api.post(`/users/${userId}/followees`);
-  };
-
-  return (
-    <React.Fragment>
-      <HeadNav />
-      <Container className={ classes.container }>
-        <ApiErrorBoundary>
-          <Typography variant="h4" className={ classes.typography }>
-            データ管理
-          </Typography>
-          {
-            user ? (
-              <>
-                <List>
-                  <CustomizedListItem
-                    icon={ <TextsmsIcon /> }
-                    headerText="ツイート"
-                    numberText={ `${user.status_count} 件` }
-                    updatedAt={ user.statuses_updated_at ? formatDateString(user.statuses_updated_at) : "-" }
-                    apiFunc={ requestTweetImport }
-                  />
-                  <CustomizedListItem
-                    icon={ <PeopleIcon /> }
-                    headerText="フォローリスト"
-                    numberText={ `${user.followee_count} 件` }
-                    updatedAt={ user.followees_updated_at ? formatDateString(user.followees_updated_at) : "-" }
-                    apiFunc={ requestFolloweeImport }
-                  />
-                </List>
-                <Divider />
-                <List>
-                  <ListItem className={ classes.deleteButtonListItem }>
-                    <ListItemSecondaryAction>
-                      <AccountDeleteDialog />
-                    </ListItemSecondaryAction>
-                  </ListItem>
-                </List>
-              </>
-            ): <CircularProgress />
-          }
-        </ApiErrorBoundary>
-      </Container>
-      <Footer bgCaramel />
-    </React.Fragment>
-  );
-};
+  }
+}
 
 export default withStyles(styles)(DataManagement);
