8752417c7edc8670edb0c6837adac17ef3f208f6
tbotaq <tbotaq@gmail.com>
2019-09-06 17:05:45 +0900

    Implement the links to external services.

---
+++ frontend/src/utils/externalUrls.js
@@ -0,0 +1,2 @@
+export const GOOGLE_ADSENSE_POLICY_URL = "https://policies.google.com/technologies/ads?hl=ja";
+export const GOOGLE_PRIVACY_POLICY_URL = "https://policies.google.com/technologies/partner-sites?hl=ja";
