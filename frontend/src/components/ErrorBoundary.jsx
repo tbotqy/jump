3388649bd5be67bb7b05059930e0c718fce92f8c
tbotaq <tbotaq@gmail.com>
2019-09-12 05:25:09 +0900

    Install Sentry to ErrorBoundary.

--- frontend/src/components/ErrorBoundary.jsx
+++ frontend/src/components/ErrorBoundary.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import * as Sentry from '@sentry/browser';
 import ErrorMessage from "./ErrorMessage";
+
+const DSN = process.env.REACT_APP_SENTRY_DSN;
+
+Sentry.init({
+  dsn: DSN,
+  environment: process.env.NODE_ENV
+})
 
 class ErrorBoundary extends React.Component {
   constructor(props) {
@@ -9,6 +17,13 @@ class ErrorBoundary extends React.Component {
 
   static getDerivedStateFromError() {
     return { hasClientError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    Sentry.withScope(scope => {
+        scope.setExtras(errorInfo);
+        Sentry.captureException(error);
+    });
   }
 
   render() {
