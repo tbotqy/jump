677fcf294c03e55fe2bebbf8337167973860d402
tbotaq <tbotaq@gmail.com>
2019-09-16 00:08:28 +0900

    Get rid of namespace.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,6 +2,4 @@
 
 # Be sure to restart your server when you modify this file.
 
-Jump::Application.config.session_store :cookie_store, key: "_jump_session"
-
-# MEMO : this file remained when 4 to 5.0 update and seems to be removed from 5.1
+Rails.application.config.session_store :cookie_store, key: "_jump_session"
