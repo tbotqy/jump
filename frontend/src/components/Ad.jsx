654d0eb769cbfc75e0a59a08bc62797b416886fc
tbotaq <tbotaq@gmail.com>
2019-09-16 20:09:07 +0900

    Disable adsense in development env.

--- frontend/src/components/Ad.jsx
+++ frontend/src/components/Ad.jsx
@@ -4,11 +4,13 @@ import AdSense from "react-adsense";
 const clientId = process.env.REACT_APP_AD_ID;
 
 const Ad = props => (
-  <AdSense.Google
-    client={ clientId }
-    slot={ props.slot }
-    responsive="true"
-  />
+  process.env.NODE_ENV === "development" ?
+    <>広告</> :
+    <AdSense.Google
+      client={ clientId }
+      slot={ props.slot }
+      responsive="true"
+    />
 );
 
 export default Ad;
