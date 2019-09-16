537b22cf8de77f5f8502eb008ce9402d74e1f537
tbotaq <tbotaq@gmail.com>
2019-09-16 21:49:10 +0900

    Fix: Respond with BadRequest for non-ajax requests.

--- app/errors/errors.rb
+++ app/errors/errors.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 module Errors
-  class Base         < StandardError; end
-  class InvalidParam < Base; end
-  class Unauthorized < Base; end
-  class NotFound     < Base; end
+  class Base               < StandardError; end
+  class InvalidParam       < Base; end
+  class InvalidRequestType < Base; end
+  class Unauthorized       < Base; end
+  class NotFound           < Base; end
 end
