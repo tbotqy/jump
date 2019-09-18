63497a8d66bdc680998c1abb237bfa6f94ac06fb
tbotaq <tbotaq@gmail.com>
2019-09-18 13:37:57 +0900

    Delegate sizing to browser.

--- frontend/src/components/top/MockUp.jsx
+++ frontend/src/components/top/MockUp.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+import { withStyles } from "@material-ui/core/styles";
 import MockUpImg from "./../../assets/mockup.png";
+
+const styles = () => ({
+  img: {
+    "width":    "auto",
+    "height":   "auto",
+    "maxWidth": "100%",
+    "maxHeight":"100%"
+  }
+})
 
 class MockUp extends React.Component {
   render() {
     return (
-      <img alt="mockup" width="100%" src={ MockUpImg } decoding="async" />
+      <img alt="mockup" src={ MockUpImg } decoding="async" className={ this.props.classes.img } />
     );
   }
 }
 
-export default MockUp;
+export default withStyles(styles)(MockUp);
