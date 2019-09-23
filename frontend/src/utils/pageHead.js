35e6ec2f0b7e67b80995b990bed3d0e85d66aeff
tbotaq <tbotaq@gmail.com>
2019-09-23 14:24:22 +0900

    Add new route for 404 page.

--- frontend/src/utils/pageHead.js
+++ frontend/src/utils/pageHead.js
@@ -5,4 +5,5 @@ export const PAGE_TITLE_TOP               = `クリックひとつで、過去�
 export const PAGE_TITLE_TERMS_AND_PRIVACY = `利用規約 プライバシーポリシー ${suffix}`;
 export const PAGE_TITLE_IMPORT            = `データのインポート ${suffix}`;
 export const PAGE_TITLE_DATA_MANAGEMENT   = `データ管理 ${suffix}`;
+export const PAGE_TITLE_NOT_FOUND         = `ページが見つかりませんでした ${suffix}`;
 export const TOP_DESCRIPTION = process.env.REACT_APP_META_DESCRIPTION;
