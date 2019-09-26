dabde9ca1d1d863713993211fd4bfd58f9afd367
tbotaq <tbotaq@gmail.com>
2019-09-26 15:09:34 +0900

    Make the threshold of slow query specifiable.

--- config/initializers/slow_query_notifier.rb
+++ config/initializers/slow_query_notifier.rb
@@ -1,11 +1,9 @@
 class SlowQueryNotifier
-  MAX_DURATION = 1.0
-
   def self.initialize!
     ActiveSupport::Notifications.subscribe('sql.active_record') do |name, start, finish, id, payload|
       duration = finish.to_f - start.to_f
 
-      if duration >= MAX_DURATION
+      if duration >= Settings.slow_query_threshold
         message = "env: `#{Rails.env}`, duration: `#{duration.floor(2)} sec`\n```#{payload[:sql]}```"
         NotifySlowQueryToSlackJob.perform_later(message: message)
       end
