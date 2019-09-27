660b87d31354afb1afad19e0dc2fb80a6a8dca8c
tbotaq <tbotaq@gmail.com>
2019-09-27 12:22:18 +0900

    Change the key of session id to disable stale cookie.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,4 +2,4 @@
 
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: "_jump_session", domain: Settings.set_cookie_domain
+Rails.application.config.session_store :cookie_store, key: "_jump_app_session", domain: Settings.set_cookie_domain
