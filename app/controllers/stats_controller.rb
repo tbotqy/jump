0c5ebb815d7c0e182b3d69351bee09f57e1b76b9
tbotaq <tbotaq@gmail.com>
2019-12-01 14:59:02 +0900

    Add the total number of users to the response of GET /stats

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -3,6 +3,10 @@
 class StatsController < ApplicationController
   def show
     status_count = StatusCount.current_count
-    render json: { status_count: status_count.to_s(:delimited) }
+    user_count   = User.count
+    render json: {
+      status_count: status_count.to_s(:delimited),
+      user_count:   user_count.to_s(:delimited)
+    }
   end
 end
--- spec/requests/stats_spec.rb
+++ spec/requests/stats_spec.rb
@@ -15,15 +15,35 @@ RSpec.describe "Stats", type: :request do
     end
 
     context "succeeds to fetch status count" do
-      let(:total_status_count) { 3 }
-      before do
-        create_list(:status, total_status_count)
-        StatusCount.count_up
-        subject
+      context "fails to fetch user count" do
+        before do
+          create_list(:status, 2)
+          StatusCount.count_up
+
+          allow(User).to receive(:count).and_raise
+          subject
+        end
+        it_behaves_like "respond with status code", :internal_server_error
       end
-      it_behaves_like "respond with status code", :ok
-      it do
-        expect(response.body).to eq({ status_count: total_status_count.to_s(:delimited) }.to_json)
+      context "succeeds to fetch user count" do
+        let(:total_user_count)   { 2 }
+        let(:total_status_count) { total_user_count }
+        before do
+          users = create_list(:user, total_user_count)
+
+          users.each { |user| create(:status, user: user) }
+          StatusCount.count_up
+
+          subject
+        end
+
+        it_behaves_like "respond with status code", :ok
+        it do
+          expect(response.body).to eq({
+            status_count: total_status_count.to_s(:delimited),
+            user_count:   total_user_count.to_s(:delimited),
+          }.to_json)
+        end
       end
     end
   end
