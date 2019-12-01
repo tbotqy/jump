65b0f47432e02a0765f9226c8ab5d746df97fcbc
tbotaq <tbotaq@gmail.com>
2019-12-01 15:13:00 +0900

    Tweak the test case to check if the returned numbers are surely delimited

--- spec/requests/stats_spec.rb
+++ spec/requests/stats_spec.rb
@@ -26,23 +26,42 @@ RSpec.describe "Stats", type: :request do
         it_behaves_like "respond with status code", :internal_server_error
       end
       context "succeeds to fetch user count" do
-        let(:total_user_count)   { 2 }
-        let(:total_status_count) { total_user_count }
-        before do
-          users = create_list(:user, total_user_count)
+        context "returned numbers are smaller than 1,000" do
+          let(:total_status_count) { total_user_count }
+          let(:total_user_count)   { 2 }
+          before do
+            users = create_list(:user, total_user_count)
 
-          users.each { |user| create(:status, user: user) }
-          StatusCount.count_up
+            users.each { |user| create(:status, user: user) }
+            StatusCount.count_up
 
-          subject
+            subject
+          end
+
+          it_behaves_like "respond with status code", :ok
+          it do
+            expect(response.body).to eq({
+              status_count: total_status_count.to_s,
+              user_count:   total_user_count.to_s,
+            }.to_json)
+          end
         end
+        context "returned numbers are bigger than 1,000" do
+          let(:total_status_count) { 2_000 }
+          let(:total_user_count)   { 1_000 }
+          before do
+            allow(User).to receive(:count).and_return(total_user_count)
+            allow(StatusCount).to receive(:current_count).and_return(total_status_count)
+            subject
+          end
 
-        it_behaves_like "respond with status code", :ok
-        it do
-          expect(response.body).to eq({
-            status_count: total_status_count.to_s(:delimited),
-            user_count:   total_user_count.to_s(:delimited),
-          }.to_json)
+          it_behaves_like "respond with status code", :ok
+          it "returns delimited numbers" do
+            expect(response.body).to eq({
+              status_count: total_status_count.to_s(:delimited),
+              user_count:   total_user_count.to_s(:delimited),
+            }.to_json)
+          end
         end
       end
     end
