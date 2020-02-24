5efd687301892d60291c963894681023386023e4
tbotaq <tbotaq@gmail.com>
2020-02-24 18:57:22 +0900

    Tweak footer's design.

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,59 +1,32 @@
 import React from "react";
 import { Link as RouterLink } from "react-router-dom";
 import {
-  Container,
   Grid,
   Link,
   Typography,
-  IconButton,
-  Theme,
-  createStyles,
-  WithStyles
+  Box
 } from "@material-ui/core";
-import TwitterIcon from "@material-ui/icons/Twitter";
-import { withStyles } from "@material-ui/core/styles";
-import {
-  ROOT_PATH,
-  TERMS_AND_PRIVACY_PATH
-} from "../utils/paths";
+import { TERMS_AND_PRIVACY_PATH } from "../utils/paths";
 import { CARAMEL_COLOR } from "../utils/colors";
 import ShareButton from "./ShareButton";
 
-const styles = (theme: Theme) => (
-  createStyles({
-    container: {
-      paddingTop: theme.spacing(5),
-      paddingBottom: theme.spacing(5),
-    },
-    link: {
-      paddingLeft: theme.spacing(2)
-    }
-  })
-);
-
 const adminTwitterScreenName = process.env.REACT_APP_ADMIN_TWITTER_SCREEN_NAME;
-const serviceDomain = process.env.REACT_APP_SERVICE_DOMAIN;
 
-interface Props extends WithStyles<typeof styles> {
+interface Props {
   bgCaramel?: boolean;
 }
 
-const Footer: React.FC<Props> = ({ bgCaramel, classes }) => {
+const Footer: React.FC<Props> = ({ bgCaramel }) => {
   const backgroundColor = bgCaramel ? CARAMEL_COLOR : "white";
 
   return (
-    <Container
-      component="footer"
-      maxWidth="xl"
-      className={ classes.container }
-      style={ { backgroundColor } }
-    >
-      <Grid container direction="column" alignItems="center" justify="center" spacing={ 2 }>
+    <Box pt={4} pb={4} style={ { backgroundColor } } component="footer">
+      <Grid container direction="column" alignItems="center" justify="center" spacing={2} style={{width:"100%"}}>
         <Grid item>
           <ShareButton />
         </Grid>
         <Grid item>
-          <Typography variant="subtitle1" color="textSecondary">
+          <Typography color="textSecondary">
             <Link
               color="inherit"
               component={ RouterLink }
@@ -64,33 +37,22 @@ const Footer: React.FC<Props> = ({ bgCaramel, classes }) => {
           </Typography>
         </Grid>
         <Grid item>
-          <Grid container direction="row" justify="center" alignItems="center" spacing={ 2 }>
+          <Grid container justify="center" spacing={2}>
             <Grid item>
-              <IconButton href={ `//twitter.com/${adminTwitterScreenName}` } target="_blank">
-                <TwitterIcon />
-              </IconButton>
+              <Typography color="textSecondary">© 2012-{ new Date().getFullYear() }</Typography>
             </Grid>
             <Grid item>
               <Typography color="textSecondary">
-                © 2012-{ new Date().getFullYear() }
-              </Typography>
-            </Grid>
-            <Grid item>
-              <Typography color="textSecondary">
-                <Link
-                  color="inherit"
-                  component={ RouterLink }
-                  to={ ROOT_PATH }
-                >
-                  { serviceDomain }
+                <Link color="inherit" href={ `//twitter.com/${adminTwitterScreenName}` } target="_blank">
+                  {`@${adminTwitterScreenName}`}
                 </Link>
               </Typography>
             </Grid>
           </Grid>
         </Grid>
       </Grid>
-    </Container>
+    </Box>
   );
 };
 
-export default withStyles(styles)(Footer);
+export default Footer;
