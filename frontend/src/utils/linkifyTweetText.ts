60c47a7e4dc8328c30942bdf64ec7b5fc6c61510
tbotaq <tbotaq@gmail.com>
2020-02-25 15:50:14 +0900

    Add rel="noopener" attr to all links on tweet body.

--- frontend/src/utils/linkifyTweetText.ts
+++ frontend/src/utils/linkifyTweetText.ts
@@ -2,6 +2,7 @@ import twitter, { AutoLinkOptions } from "twitter-text";
 import { filterXSS } from "xss";
 import { UrlEntity } from "../api";
 
+const addNoOpenerToAnchors = (html: string): string => html.replace(/(<a.*?)>/g, '$1 rel="noopener">');
 
 const autoLinkOptions: AutoLinkOptions = {
   targetBlank: true,
@@ -9,17 +10,21 @@ const autoLinkOptions: AutoLinkOptions = {
   usernameIncludeSymbol: true
 };
 
+const filterXSSOptions = {
+  whiteList: {
+    a: ["href", "target", "rel"]
+  }
+};
+
 export default function linkifyTweetText(text: string, urlEntities: UrlEntity[]): string {
   let html: string = twitter.autoLink(text, autoLinkOptions);
   urlEntities.forEach( urlEntity => {
-    html = html.replace(`>${urlEntity.url}</a>`, ` rel="noopener">${urlEntity.displayUrl}</a>`);
+    html = html.replace(`>${urlEntity.url}</a>`, `>${urlEntity.displayUrl}</a>`);
   });
 
-  html = filterXSS(html, {
-    whiteList: {
-      a: ["href", "target", "rel"]
-    }
-  });
+  html = addNoOpenerToAnchors(html);
+
+  html = filterXSS(html, filterXSSOptions);
 
   return html;
 }
