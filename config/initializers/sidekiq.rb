e0e7655c0971c4b4614e13c14b139dbd44f9b03f
tbotaq <140096+tbotaq@users.noreply.github.com>
2020-09-03 23:09:31 +0900

    Sync the log level of Sidekiq with Rails.logger . (#907)

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,10 +2,12 @@
 
 Sidekiq.configure_server do |config|
   config.redis = { url: Settings.redis.endpoint, namespace: Settings.sidekiq.namespace }
+  config.logger.level = Rails.logger.level
 end
 
 Sidekiq.configure_client do |config|
   config.redis = { url: Settings.redis.endpoint, namespace: Settings.sidekiq.namespace }
+  config.logger.level = Rails.logger.level
 end
 
 schedule_file = "config/sidekiq_cron_schedule.yml"
